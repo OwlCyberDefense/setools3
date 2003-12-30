7f4df9be4e41d546f2ea1e561740b0eacea88242
!donp <!donp@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2003-12-30 21:41:15 +0000

    changed seuser version number back to 1.0; think this was already changed by Frank. Not sure because did not have the -WORKING tag appended to it.
    2003/12/12 22:50:37-00:00 !donp
    version number updates; apol help file update; fulflow.tcl update; other minor fixes
    
    2003/10/10 18:23:23-00:00 !mayerf
    Coomprehensive update that primarily implements the creation of two libseuser libs; one core C lib and one that adds the TCL support.  This required extensive changes to the seuser command line program, and the entire removal of the wrapper layer.  It also had ancillary impact on libapol, and on the make process for all tools.
    
    2003/04/10 13:23:22-00:00 !mayerf
    Various minor cleanups and comments changes as part of final pre-release review
    
    2003/04/04 14:36:16-00:00 !mayerf
    libseuser changes to support latest seuser command line changes
    
    2003/03/28 21:46:35-00:00 !mayerf
    Changes to libseuser to support new seuser command line options
    
    2002/05/16 01:32:01-00:00 !mayerf
    Initial revision
    
    (Logical change 1.3)

---
+++ libseuser/seuser_db.h
@@ -0,0 +1,65 @@
+ /* Copyright (C) 2002-2003 Tresys Technology, LLC
+ * see file 'COPYING' for use and warranty information */
+
+/* 
+ * Author: mayerf@tresys.com 
+ */
+
+/* seuser_db.h
+ *
+ * The user database functions for seuser.
+ *
+ */
+#ifndef _SEUSER_DB_H_
+#define _SEUSER_DB_H_
+
+#include "../libapol/policy.h"
+
+#define SEUSER_VERSION_STRING "1.0"
+/* NOTE: As of version 1.0, we completely remove all support for the older
+ * default_context and cron_context; which greatly simplified the code.
+ * Also structure lib so that it no longer requires the TCL interface
+ * to be used.
+ */
+
+#define CONFIG_FILE	"seuser.conf"
+
+/* user database structure; based in part on APOL's structures */
+typedef struct user_db {
+	int		num_users;
+	user_list_t	users;
+	/* stash the conf file info here */
+	bool_t		conf_init;	/* indicate whether config data is initialized */
+	char		*config_dir;
+	char 		*policy_conf;
+	char 		*user_file;
+	char 		*policy_dir;
+} user_db_t;
+
+/* macros */
+#define seuser_is_conf_loaded(db) (db == NULL ? FALSE : (db)->conf_init)
+
+/* prototypes */
+int seuser_open_user_db(user_db_t *db, policy_t **policy);
+int seuser_get_user_by_name(const char *name, user_item_t **user, user_db_t *db);
+int seuser_get_user_context(const char *user, int which, char **role, char **type, user_db_t *db, policy_t *policy);
+int seuser_write_user_file(user_db_t *db, policy_t *policy);
+int seuser_write_default_context(int which, user_db_t *db, policy_t *policy, FILE *fp);
+int seuser_update_user(user_item_t *user, bool_t newuser, user_db_t *db, policy_t *policy);
+int seuser_remove_user(const char *name, user_db_t *db);
+int seuser_is_proper_user_record(user_item_t *user, user_db_t *db, policy_t *policy);
+int seuser_free_db(user_db_t *db, bool_t free_conf);
+int seuser_init_db(user_db_t *db, bool_t init_conf);
+int seuser_rename_user(const char *oldname, const char *newname, user_db_t *db);
+bool_t seuser_does_user_exist(const char *name, user_db_t *db);
+int seuser_read_conf_info(user_db_t *db);
+int seuser_remake_policy_conf(const char *tmpfile, user_db_t *db);
+int seuser_reinstall_policy(const char *tmpfile, user_db_t *db);
+int seuser_check_commit_perm(user_db_t *db);
+const char* seuser_decode_read_conf_err(int err);
+int seuser_add_change_user(bool_t new_user, const char *user, char ** roles, int num_roles, user_db_t *db, policy_t *policy);
+
+#endif /*_SEUSER_DB_H_ */
+
+
+
