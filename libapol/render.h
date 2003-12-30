314afca68c173b08c95b030849826f0c3d75f30c
!mayerf <!mayerf@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2003-12-30 21:41:15 +0000

    Various minor cleanups and comments changes as part of final pre-release review
    2003/03/12 21:27:44-00:00 !mayerf
    Large interim check-in, primarily changes to libapol to support that new domain transition analysis.
    
    (Logical change 1.3)

---
+++ libapol/render.h
@@ -0,0 +1,22 @@
+ /* Copyright (C) 2003 Tresys Technology, LLC
+ * see file 'COPYING' for use and warranty information */
+
+/* 
+ * Author: mayerf@tresys.com 
+ */
+
+/* render.h */
+
+/* Utility functions to render aspects of a policy into strings */
+
+#ifndef _APOLICY_RENDER_H_
+#define _APOLICY_RENDER_H__
+
+#include <stdlib.h>
+#include "policy.h"
+
+char *re_render_av_rule(bool_t addlineno, int idx, bool_t is_au, policy_t *policy) ;
+char *re_render_tt_rule(bool_t addlineno, int idx, policy_t *policy) ;
+
+
+#endif /*_APOLICY_RENDER_H_*/
