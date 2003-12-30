aa3980c9c0725d5f972b2fee34b67c7f8eeebcbd
Frank Mayer <mayerf@tresys.com>
2003-12-30 21:41:15 +0000

    Checked in first version of the new updated GUI for seuser
    (Logical change 1.3)

---
+++ seuser/seuser_tail.tcl
@@ -0,0 +1,17 @@
+#  tail.tcl (entry point)
+#
+# This is the end of the seuser tool script file (se_user.tcl)
+#
+# -----------------------------------------------------------
+# Copyright (C) 2003 Tresys Technology, LLC
+# see file 'COPYING' for use and warranty information 
+#
+# Requires tcl and tk 8.3+, with BWidgets
+# Author: <don.patterson@tresys.com, mayerf@tresys.com>
+# -----------------------------------------------------------   
+##############################################################
+# ::Seuser main call
+#######################################################
+# Start script here
+SEUser_Top::main 
+
