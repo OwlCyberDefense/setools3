a922cf6ea35710cc87181cc08d5b1cc45384fc49
tresys.com!kmacmillan <tresys.com!kmacmillan@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-01-06 22:25:17 +0000

    Fix warnings for Tcl 8.3. This requires casting const char * to char *, but this is just because of the broken Tcl headers.
    (Logical change 1.24)

--- setools/libapol/apol_tcl.c
+++ setools/libapol/apol_tcl.c
@@ -1266,7 +1266,7 @@ int Apol_ClosePolicy(ClientData clientData, Tcl_Interp *interp, int argc, char *
 
 int Apol_GetVersion(ClientData clientData, Tcl_Interp *interp, int argc, char *argv[])
 {
-	Tcl_AppendResult(interp, libapol_get_version(), (char *) NULL);
+	Tcl_AppendResult(interp, (char*)libapol_get_version(), (char *) NULL);
 	return TCL_OK;
 }
 
@@ -3831,7 +3831,7 @@ int Apol_Init(Tcl_Interp *interp)
 	Tcl_CreateCommand(interp, "apol_TransitiveFindPathsGetResults", (Tcl_CmdProc *) Apol_TransitiveFindPathsGetResults, (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
 	Tcl_CreateCommand(interp, "apol_TransitiveFindPathsAbort", (Tcl_CmdProc *) Apol_TransitiveFindPathsAbort, (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL);
 	
-	Tcl_PkgProvide(interp, "apol", libapol_get_version());
+	Tcl_PkgProvide(interp, "apol", (char*)libapol_get_version());
 
 	return TCL_OK;
 }
