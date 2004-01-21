362cae79e31c42e96f5ddf934867b9a471f22a0b
Kevin Carr <kcarr@tresys.com>
2004-01-21 17:11:53 +0000

    added top_filters_view_t data structure to manage multiple views
    (Logical change 1.64)

--- setools/seaudit/filter_window.h
+++ setools/seaudit/filter_window.h
@@ -13,6 +13,7 @@
 #ifndef SEAUDIT_FILTER_WINDOW_H
 #define SEAUDIT_FILTER_WINDOW_H
 
+#include "auditlogmodel.h"
 #include <glib.h>
 #include <gtk/gtk.h>
 #include <glade/glade.h>
@@ -36,11 +37,23 @@ typedef struct filters {
 	GladeXML *xml;
 } filters_t;
 
+typedef struct top_filters_view {
+	filters_t *filters;
+	SEAuditLogViewStore *store;
+	GtkTreeView *tree_view;
+	gint notebook_index;
+} top_filters_view_t;
+ 
 /***************************
  * Public member functions *
  ***************************/
+top_filters_view_t* top_filters_view_create(audit_log_t *log, GtkTreeView *tree_view);
+void top_filters_view_set_log(top_filters_view_t *view, audit_log_t *log);
+void top_filters_view_display(top_filters_view_t* filters_view);
+void top_filters_view_set_notebook_index(top_filters_view_t *filters_view, gint index);
+void top_filters_view_do_filter(gpointer data, gpointer user_data); /* used as callback from g_list_foreach() */
+
 filters_t* filters_create(void);
 void filters_destroy(filters_t* filters);
-void filters_display(filters_t* filters);
 
 #endif
