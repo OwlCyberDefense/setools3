7ca830c898702b412ebdf44d7f9e2aa784749f6c
tresys.com!kcarr <tresys.com!kcarr@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-01-23 18:35:03 +0000

    moved data definitions from filter_window.c
    (Logical change 1.65)

--- seaudit/filter_window.h
+++ seaudit/filter_window.h
@@ -8,6 +8,7 @@
  * Modified by Don Patterson <don.patterson@tresys.com>
  * and Karl MacMillan <kmacmillan@tresys.com>
  *
+ *
  */
 
 #ifndef SEAUDIT_FILTER_WINDOW_H
@@ -18,16 +19,56 @@
 #include <gtk/gtk.h>
 #include <glade/glade.h>
 
-struct filters_select_items;
+enum {
+	ITEMS_LIST_COLUMN, 
+	NUMBER_ITEMS_LIST_COLUMNS
+};
+
+enum items_list_types_t {
+	SEAUDIT_SRC_TYPES,
+	SEAUDIT_SRC_USERS,
+	SEAUDIT_SRC_ROLES,
+	SEAUDIT_TGT_TYPES,
+	SEAUDIT_TGT_USERS,
+	SEAUDIT_TGT_ROLES,
+	SEAUDIT_OBJECTS
+};
+
+enum select_values_source_t {
+	SEAUDIT_FROM_LOG,
+	SEAUDIT_FROM_POLICY,
+	SEAUDIT_FROM_UNION
+};
+
+typedef struct  seaudit_filter_list {
+	char **list;
+	int size;
+} seaudit_filter_list_t;
+
+void filters_seaudit_filter_list_free(seaudit_filter_list_t *list);
+
+struct filters;
+
+typedef struct filters_select_items {
+	GtkListStore *selected_items;
+	GtkListStore *unselected_items;
+	enum items_list_types_t items_list_type;
+        enum select_values_source_t items_source;
+	GtkWindow *window;
+	GladeXML *xml;
+	struct filters *parent;
+} filters_select_items_t;
+
+void filters_select_items_parse_entry(filters_select_items_t *s);
 
 typedef struct filters {
-	struct filters_select_items *src_types_items;
-	struct filters_select_items *src_users_items;
-	struct filters_select_items *src_roles_items;
-	struct filters_select_items *tgt_types_items;
-	struct filters_select_items *tgt_users_items;
-	struct filters_select_items *tgt_roles_items;
-	struct filters_select_items *obj_class_items;
+	filters_select_items_t *src_types_items;
+	filters_select_items_t *src_users_items;
+	filters_select_items_t *src_roles_items;
+	filters_select_items_t *tgt_types_items;
+	filters_select_items_t *tgt_users_items;
+	filters_select_items_t *tgt_roles_items;
+	filters_select_items_t *obj_class_items;
 	GString *ip_address;
 	GString *port;
 	GString *interface;
@@ -37,23 +78,12 @@ typedef struct filters {
 	GladeXML *xml;
 } filters_t;
 
-typedef struct top_filters_view {
-	filters_t *filters;
-	SEAuditLogViewStore *store;
-	GtkTreeView *tree_view;
-	gint notebook_index;
-} top_filters_view_t;
- 
 /***************************
  * Public member functions *
  ***************************/
-top_filters_view_t* top_filters_view_create(audit_log_t *log, GtkTreeView *tree_view);
-void top_filters_view_set_log(top_filters_view_t *view, audit_log_t *log);
-void top_filters_view_display(top_filters_view_t* filters_view);
-void top_filters_view_set_notebook_index(top_filters_view_t *filters_view, gint index);
-void top_filters_view_do_filter(gpointer data, gpointer user_data); /* used as callback from g_list_foreach() */
-
 filters_t* filters_create(void);
 void filters_destroy(filters_t* filters);
+void filters_display(filters_t* filters);
+seaudit_filter_list_t* filters_seaudit_filter_list_get(filters_select_items_t *filters_select_item);
 
 #endif
