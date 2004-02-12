4658e3fbdb497a65f052881f7639b1b2a4dc7cd8
tresys.com!kcarr <tresys.com!kcarr@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-02-12 23:14:31 +0000

    altered to work with multiple filters
    (Logical change 1.97)

--- setools/seaudit/filtered_view.c
+++ setools/seaudit/filtered_view.c
@@ -12,9 +12,7 @@
 #include "utilgui.h"
 #include <string.h>
 
-static void filtered_view_on_do_filter_button_clicked(GtkButton *button, seaudit_filtered_view_t *filtered_view);
-
-seaudit_filtered_view_t* seaudit_filtered_view_create(audit_log_t *log, GtkTreeView *tree_view)
+seaudit_filtered_view_t* seaudit_filtered_view_create(audit_log_t *log, GtkTreeView *tree_view, const char *view_name)
 {
 	seaudit_filtered_view_t *filtered_view;
 
@@ -27,19 +25,20 @@ seaudit_filtered_view_t* seaudit_filtered_view_create(audit_log_t *log, GtkTreeV
 		return NULL;
 	}
 
-	if ((filtered_view->filters = filters_create()) == NULL) {
+	if ((filtered_view->multifilter_window = multifilter_window_create(filtered_view, view_name)) == NULL) {
 		fprintf(stderr, "out of memory");
 		free(filtered_view);
 		return NULL;
 	}
 	if ((filtered_view->store = seaudit_log_view_store_create()) == NULL) {
 		fprintf(stderr, "out of memory");
-		free(filtered_view->filters);
+		free(filtered_view->multifilter_window);
 		free(filtered_view);
 		return NULL;
 	}
 	filtered_view->notebook_index = -1;
 	filtered_view->tree_view = tree_view;
+	filtered_view->name = g_string_new(view_name);
 	seaudit_log_view_store_open_log(filtered_view->store, log);
 	gtk_tree_view_set_model(tree_view, GTK_TREE_MODEL(filtered_view->store));
 
@@ -48,21 +47,16 @@ seaudit_filtered_view_t* seaudit_filtered_view_create(audit_log_t *log, GtkTreeV
 
 void seaudit_filtered_view_destroy(seaudit_filtered_view_t *view)
 {
-	filters_destroy(view->filters);
+	multifilter_window_destroy(view->multifilter_window);
 	seaudit_log_view_store_close_log(view->store);
+	g_string_free(view->name, TRUE);
 }
 
 void seaudit_filtered_view_display(seaudit_filtered_view_t* filtered_view)
 {
-	if (filtered_view->filters->window != NULL) {
-		gtk_window_present(filtered_view->filters->window); 
+	if(!filtered_view)
 		return;
-	}
-
-	filters_display(filtered_view->filters);
-	glade_xml_signal_connect_data(filtered_view->filters->xml, "filtered_view_on_do_filter_button_clicked",
-				      G_CALLBACK(filtered_view_on_do_filter_button_clicked),
-				      filtered_view);
+	multifilter_window_display(filtered_view->multifilter_window);
 }
 
 void seaudit_filtered_view_set_log(seaudit_filtered_view_t *view, audit_log_t *log)
@@ -85,166 +79,4 @@ void seaudit_filtered_view_do_filter(seaudit_filtered_view_t *view, gpointer use
 	seaudit_log_view_store_do_filter(view->store);
 }
 
-static void filtered_view_on_do_filter_button_clicked(GtkButton *button, seaudit_filtered_view_t *filtered_view)
-{
-	GtkWidget *widget;
-	GtkWidget *window;
-	GtkTreeIter iter;
-	GladeXML *xml;
-	SEAuditLogViewStore *store;
-	seaudit_filter_list_t *items_list = NULL;
-	filter_t *filter;
-	char *text;
-	int int_val;
-	
-	window = GTK_WIDGET(filtered_view->filters->window);
-	g_assert(window);
-	show_wait_cursor(window);	
-	
-	audit_log_view_purge_filters(filtered_view->store->log_view);
-	store = filtered_view->store;
 
-	/* Result message value */
-	xml = filtered_view->filters->xml;
-	widget = glade_xml_get_widget(xml, "ResultComboEntry");
-	text = (char*)gtk_entry_get_text(GTK_ENTRY(widget));
-	if (strcmp(text, "SHOW messages that match ALL criteria") == 0) {
-		store->log_view->fltr_out = FALSE;
-		store->log_view->fltr_and = TRUE;
-	} else if (strcmp(text, "SHOW messages that match ANY criteria") == 0) {
-		store->log_view->fltr_out = FALSE;
-		store->log_view->fltr_and = FALSE;
-	} else if (strcmp(text, "HIDE messages that match ALL criteria") == 0) {
-		store->log_view->fltr_out = TRUE;
-		store->log_view->fltr_and = TRUE;
-	} else if (strcmp(text, "HIDE messages that match ANY criteria") == 0) {
-		store->log_view->fltr_out = TRUE;
-		store->log_view->fltr_and = FALSE;
-	} else {
-		message_display(GTK_WINDOW(window), GTK_MESSAGE_ERROR, "Invalid results message combobox value.\n");
-		return;		
-	}
-	
-	/* check for src type filter */
-	filters_select_items_parse_entry(filtered_view->filters->src_types_items);
-	if (gtk_tree_model_get_iter_first(GTK_TREE_MODEL(filtered_view->filters->src_types_items->selected_items), &iter)) {
-		items_list = filters_seaudit_filter_list_get(filtered_view->filters->src_types_items);
-		if (items_list == NULL) 
-			return;
-		filter = src_type_filter_create(items_list->list, items_list->size);
-		filters_seaudit_filter_list_free(items_list);
-		audit_log_view_add_filter(store->log_view, filter);	
-	}
-
-	/* check for tgt type filter */
-	filters_select_items_parse_entry(filtered_view->filters->tgt_types_items);
-	if (gtk_tree_model_get_iter_first(GTK_TREE_MODEL(filtered_view->filters->tgt_types_items->selected_items), &iter)) {
-		items_list = filters_seaudit_filter_list_get(filtered_view->filters->tgt_types_items);
-		if (items_list == NULL) 
-			return;
-		filter = tgt_type_filter_create(items_list->list, items_list->size);
-		filters_seaudit_filter_list_free(items_list);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-	/* check for obj class filter */
-	filters_select_items_parse_entry(filtered_view->filters->obj_class_items);
-	if (gtk_tree_model_get_iter_first(GTK_TREE_MODEL(filtered_view->filters->obj_class_items->selected_items), &iter)) {
-		items_list = filters_seaudit_filter_list_get(filtered_view->filters->obj_class_items);
-		if (items_list == NULL) 
-			return;
-		filter = class_filter_create(items_list->list, items_list->size);
-		filters_seaudit_filter_list_free(items_list);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-
-	/* check for src user filter */
-	filters_select_items_parse_entry(filtered_view->filters->src_users_items);
-	if (gtk_tree_model_get_iter_first(GTK_TREE_MODEL(filtered_view->filters->src_users_items->selected_items), &iter)) {
-		items_list = filters_seaudit_filter_list_get(filtered_view->filters->src_users_items);
-		if (items_list == NULL) 
-			return;
-		filter = src_user_filter_create(items_list->list, items_list->size);
-		filters_seaudit_filter_list_free(items_list);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-
-	/* check for src role filter */
-	filters_select_items_parse_entry(filtered_view->filters->src_roles_items);
-	if (gtk_tree_model_get_iter_first(GTK_TREE_MODEL(filtered_view->filters->src_roles_items->selected_items), &iter)) {
-		items_list = filters_seaudit_filter_list_get(filtered_view->filters->src_roles_items);
-		if (items_list == NULL) 
-			return;
-		filter = src_role_filter_create(items_list->list, items_list->size);
-		filters_seaudit_filter_list_free(items_list);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-
-	/* check for tgt user filter */
-	filters_select_items_parse_entry(filtered_view->filters->tgt_users_items);
-	if (gtk_tree_model_get_iter_first(GTK_TREE_MODEL(filtered_view->filters->tgt_users_items->selected_items), &iter)) {
-		items_list = filters_seaudit_filter_list_get(filtered_view->filters->tgt_users_items);
-		if (items_list == NULL) 
-			return;
-		filter = tgt_user_filter_create(items_list->list, items_list->size);
-		filters_seaudit_filter_list_free(items_list);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-
-	/* check for tgt role filter */
-	filters_select_items_parse_entry(filtered_view->filters->tgt_roles_items);
-	if (gtk_tree_model_get_iter_first(GTK_TREE_MODEL(filtered_view->filters->tgt_roles_items->selected_items), &iter)) {
-		items_list = filters_seaudit_filter_list_get(filtered_view->filters->tgt_roles_items);
-		if (items_list == NULL) 
-			return;
-		filter = tgt_role_filter_create(items_list->list, items_list->size);
-		filters_seaudit_filter_list_free(items_list);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-	
-	/* check for network address filter */
-	widget = glade_xml_get_widget(xml, "IPAddressEntry");
-	text = (char*)gtk_entry_get_text(GTK_ENTRY(widget));
-	if (strcmp(text, "") != 0) {
-		filter = ipaddr_filter_create(text);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-
-	/* check for network port filter */
-	widget = glade_xml_get_widget(xml, "PortEntry");
-	text = (char*)gtk_entry_get_text(GTK_ENTRY(widget));
-	if (strcmp(text, "") != 0) {
-		int_val = atoi(text);
-		filter = ports_filter_create(int_val);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-	
-	/* check for network interface filter */
-	widget = glade_xml_get_widget(xml, "InterfaceEntry");
-	text = (char*)gtk_entry_get_text(GTK_ENTRY(widget));
-	if (strcmp(text, "") != 0) {
-		filter = netif_filter_create(text);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-	
-	/* check for executable filter */
-	widget = glade_xml_get_widget(xml, "ExeEntry");
-	text = (char*)gtk_entry_get_text(GTK_ENTRY(widget));
-	if (strcmp(text, "") != 0) {
-		filter = exe_filter_create(text);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-	
-	/* check for path filter */
-	widget = glade_xml_get_widget(xml, "PathEntry");
-	text = (char*)gtk_entry_get_text(GTK_ENTRY(widget));
-	if (strcmp(text, "") != 0) {
-		filter = path_filter_create(text);
-		audit_log_view_add_filter(store->log_view, filter);
-	}
-	
-	show_wait_cursor(window);
-
-	/* do the filter on the model */
- 	seaudit_log_view_store_do_filter(store);
-	clear_wait_cursor(window);
-}
