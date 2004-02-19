9c6226f885222545c6598b92f569eb1d0ef8c18c
tresys.com!kcarr <tresys.com!kcarr@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-02-19 16:34:16 +0000

    added saving and loading filters in libseaudit
    (Logical change 1.109)

--- libseaudit/filters.h
+++ libseaudit/filters.h
@@ -36,14 +36,20 @@ typedef struct seaudit_filter {
 	seaudit_criteria_t *ipaddr_criteria;
 	seaudit_criteria_t *ports_criteria;
 	enum seaudit_filter_match_t match;
+	char *name;
+	char *desc;
 } seaudit_filter_t;
 
 seaudit_filter_t* seaudit_filter_create(void);  /* create and init */
 void seaudit_filter_init(seaudit_filter_t *seaudit_filter);
 void seaudit_filter_destroy(seaudit_filter_t *seaudit_filter);
 void seaudit_filter_set_match(seaudit_filter_t *seaudit_filter, enum seaudit_filter_match_t match);
+void seaudit_filter_set_name(seaudit_filter_t *seaudit_filter, const char *name);
+void seaudit_filter_set_desc(seaudit_filter_t *seaudit_filter, const char *desc);
 void seaudit_filter_make_dirty_criterias(seaudit_filter_t *seaudit_filter);
 bool_t seaudit_filter_does_message_match(seaudit_filter_t *filter, msg_t *message, audit_log_t *log);
+int seaudit_filter_save_to_file(seaudit_filter_t *filter, const char *filename);
+int seaudit_filter_load_from_file(seaudit_filter_t **filter, const char *filename);
 llist_t* seaudit_filter_get_list(seaudit_filter_t *filter);
 
 #endif
