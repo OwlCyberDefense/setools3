6831cc1645b4804adb024fc626d35f5e2ae19f3d
Kevin Carr <kcarr@tresys.com>
2004-02-26 17:45:17 +0000

    added hostname filtering
    2004/02/25 14:10:03-05:00 tresys.com!kcarr
    moved loading filters into multifilter
    
    (Logical change 1.124)

--- setools/libseaudit/filters.c
+++ setools/libseaudit/filters.c
@@ -13,237 +13,10 @@
 #include "filters.h"
 #include <string.h>
 #include <libapol/util.h>
-#include <libxml/parser.h>
 #include <libxml/uri.h>
 
 static void dummy_free(void *foo) {  }
 
-/* xml parser data structures */
-enum seaudit_filter_parser_state_t {
-	PARSING_NONE,
-	PARSING_SRC_TYPES,
-	PARSING_TGT_TYPES,
-	PARSING_SRC_USERS,
-	PARSING_TGT_USERS,
-	PARSING_SRC_ROLES,
-	PARSING_TGT_ROLES,
-	PARSING_CLASSES,
-	PARSING_EXE,
-	PARSING_PATH,
-	PARSING_NETIF,
-	PARSING_IPADDR,
-	PARSING_PORTS,
-	PARSING_DESC
-};
-	       
-typedef struct seaudit_filter_parser_data {
-	seaudit_filter_t *filter;
-	enum seaudit_filter_parser_state_t state;
-	bool_t parsing_item;
-	char **strs; /* parser data */
-	int num_strs;
-} seaudit_filter_parser_data_t;
-
-static void seaudit_filter_parser_data_free(seaudit_filter_parser_data_t *data)
-{
-	int i;
-
-	if (data->strs) {
-		for (i = 0; i < data->num_strs; i++)
-			if (data->strs[i])
-				free(data->strs[i]);
-		free(data->strs);
-		data->strs = NULL;
-		data->num_strs = 0;
-	}
-}
-
-/* implementation of xml parser callback functions */
-static void my_parse_characters(void *user_data, const xmlChar *ch, int len)
-{
-	seaudit_filter_parser_data_t *data = (seaudit_filter_parser_data_t *)user_data;
-
-	switch(data->state) {
-	case PARSING_NONE:
-		break;
-	case PARSING_SRC_TYPES:
-	case PARSING_TGT_TYPES:
-	case PARSING_SRC_ROLES:
-	case PARSING_TGT_ROLES:
-	case PARSING_SRC_USERS:
-	case PARSING_TGT_USERS:
-	case PARSING_CLASSES:
-	case PARSING_EXE:
-	case PARSING_PATH:
-	case PARSING_NETIF:
-	case PARSING_IPADDR:
-	case PARSING_PORTS:
-		if (!data->parsing_item)
-			break;
-	case PARSING_DESC:
-		data->strs = (char**)realloc(data->strs, sizeof(char*)*((data->num_strs)+2));
-		data->strs[data->num_strs] = xmlURIUnescapeString(ch, len, NULL);
-		data->strs[data->num_strs+1] = NULL;
-		data->num_strs++;
-		break;
-	}
-}
-
-static void my_parse_endElement(void *user_data, const xmlChar *name)
-{
-	seaudit_filter_parser_data_t *data = (seaudit_filter_parser_data_t *)user_data;
-
-	if (!data->strs)
-		return;
-
-	if (strcmp(name, "desc") == 0) {
-		if (data->strs[0])
-			seaudit_filter_set_desc(data->filter, data->strs[0]);
-		seaudit_filter_parser_data_free(data);
-		data->state = PARSING_NONE;
-		return;
-	}
-
-	if (strcmp(name, "item") == 0) {
-		data->parsing_item = FALSE;
-		return;
-	}
-
-	if (strcmp(name, "criteria") == 0) {
-		switch (data->state) {
-		case PARSING_NONE:
-		case PARSING_DESC: /* should never get here */
-			break;
-		case PARSING_SRC_TYPES:
-			data->filter->src_type_criteria = src_type_criteria_create(data->strs, data->num_strs);
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_TGT_TYPES:
-			data->filter->tgt_type_criteria = tgt_type_criteria_create(data->strs, data->num_strs);
-			seaudit_filter_parser_data_free(data);		
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_SRC_ROLES:
-			data->filter->src_role_criteria = src_role_criteria_create(data->strs, data->num_strs);
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_TGT_ROLES:
-			data->filter->tgt_role_criteria = tgt_role_criteria_create(data->strs, data->num_strs);
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_SRC_USERS:
-			data->filter->src_user_criteria = src_user_criteria_create(data->strs, data->num_strs);
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_TGT_USERS:
-			data->filter->tgt_user_criteria = tgt_user_criteria_create(data->strs, data->num_strs);
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_CLASSES:
-			data->filter->class_criteria = class_criteria_create(data->strs, data->num_strs);
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_EXE:
-			if (data->strs[0])
-				data->filter->exe_criteria = exe_criteria_create(data->strs[0]);
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_PATH:
-			if (data->strs[0])
-				data->filter->path_criteria = path_criteria_create(data->strs[0]);
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_NETIF:
-			if (data->strs[0])
-				data->filter->netif_criteria = netif_criteria_create(data->strs[0]);
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_IPADDR:
-			if (data->strs[0])
-				data->filter->ipaddr_criteria = ipaddr_criteria_create(data->strs[0]);
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-			break;
-		case PARSING_PORTS:
-			if (data->strs[0])
-				data->filter->ports_criteria = ports_criteria_create(atoi(data->strs[0]));
-			seaudit_filter_parser_data_free(data);
-			data->state = PARSING_NONE;
-		}
-	}
-
-}
-
-static void my_parse_startElement(void *user_data, const xmlChar *name, const xmlChar **attrs)
-{
-	seaudit_filter_parser_data_t *data = (seaudit_filter_parser_data_t *)user_data;	
-	char *unescaped;
-
-        /* set state and process attributes.
-	 * attributes are passed in by name value pairs. */
-	if (strcmp(name, "filter") == 0) {
-		data->state = PARSING_NONE;
-		if (!attrs[0] || !attrs[1] || !attrs[2] || !attrs[3])
-			return;
-		if (strcmp(attrs[0], "name") == 0)
-			if (data->filter->name)
-				free(data->filter->name);
-		unescaped = xmlURIUnescapeString(attrs[1], -1, NULL);
-		data->filter->name = strdup(unescaped);
-		free(unescaped);
-		if (strcmp(attrs[2], "match") == 0) {
-			if (strcmp(attrs[3], "all") == 0)
-				data->filter->match = SEAUDIT_FILTER_MATCH_ALL;
-			else 
-				data->filter->match = SEAUDIT_FILTER_MATCH_ANY;
-		}
-
-	} else if (strcmp(name, "desc") == 0) {
-		data->state = PARSING_DESC;
-
-	} else if (strcmp(name, "criteria") == 0) {
-		if (!attrs[0] || !attrs[1] || strcmp(attrs[0], "type") != 0)
-			data->state = PARSING_NONE;
-		else if (strcmp(attrs[1], "src_type") == 0)
-			data->state = PARSING_SRC_TYPES;
-		else if (strcmp(attrs[1], "tgt_type") == 0)
-			data->state = PARSING_TGT_TYPES;
-		else if (strcmp(attrs[1], "src_user") == 0)
-			data->state = PARSING_SRC_USERS;
-		else if (strcmp(attrs[1], "tgt_user") == 0)
-			data->state = PARSING_TGT_USERS;
-		else if (strcmp(attrs[1], "src_role") == 0)
-			data->state = PARSING_SRC_ROLES;
-		else if (strcmp(attrs[1], "tgt_role") == 0)
-			data->state = PARSING_TGT_ROLES;
-		else if (strcmp(attrs[1], "obj_class") == 0)
-			data->state = PARSING_CLASSES;
-		else if (strcmp(attrs[1], "exe") == 0)
-			data->state = PARSING_EXE;
-		else if (strcmp(attrs[1], "path") == 0)
-			data->state = PARSING_PATH;
-		else if (strcmp(attrs[1], "netif") == 0)
-			data->state = PARSING_NETIF;
-		else if (strcmp(attrs[1], "ipaddr") == 0)
-			data->state = PARSING_IPADDR;
-		else if (strcmp(attrs[1], "port") == 0)
-			data->state = PARSING_PORTS;
-		else
-			data->state = PARSING_NONE;
-
-        } else if (strcmp(name, "item") == 0) {
-		data->parsing_item = TRUE;
-	}
-}
 
 seaudit_filter_t* seaudit_filter_create(void)
 {
@@ -353,6 +126,8 @@ bool_t seaudit_filter_does_message_match(seaudit_filter_t *filter, msg_t *messag
 	}
 	if (filter->match == SEAUDIT_FILTER_MATCH_ANY)
 		match = FALSE;
+	if (filter->match == SEAUDIT_FILTER_MATCH_ALL)
+		match = TRUE;
 	ll_free(list, dummy_free);
 	return match;
 }
@@ -373,6 +148,7 @@ llist_t* seaudit_filter_get_list(seaudit_filter_t *filter)
 	ll_append_data(list, filter->netif_criteria);
 	ll_append_data(list, filter->ipaddr_criteria);
 	ll_append_data(list, filter->ports_criteria);
+	ll_append_data(list, filter->host_criteria);
 	return list;
 }
 
@@ -380,7 +156,6 @@ int seaudit_filter_save_to_file(seaudit_filter_t *filter, const char *filename)
 {
 	FILE *file;
 	const char *XML_VER = "<?xml version=\"1.0\"?>\n";
-	char *escaped;
 
 	if (!filter || !filename)
 		return -1;
@@ -388,59 +163,47 @@ int seaudit_filter_save_to_file(seaudit_filter_t *filter, const char *filename)
 	if (!file)
 		return -1;
 	fprintf(file, XML_VER);
-	fprintf(file, "<structure xmlns=\"http://www.tresys.com/setools/seaudit/%s/\">\n", 
-		LIBSEAUDIT_VERSION_STRING);
+	fprintf(file, "<view xmlns=\"http://www.tresys.com/setools/seaudit/%s/\">\n", 
+		FILTER_FILE_FORMAT_VERSION);
+	seaudit_filter_append_to_file(filter, file, 1);
+	fprintf(file, "</view>\n");
+	fclose(file);	
+	return 0;
+}
+
+void seaudit_filter_append_to_file(seaudit_filter_t *filter, FILE *file, int tabs)
+{
+	seaudit_criteria_t *criteria;
+	llist_t *list;
+	llist_node_t *node;
+	char *escaped;
+	int i;
+
+	if (!filter || !file)
+		return;
+
 	escaped = xmlURIEscapeStr(filter->name, NULL);
-	fprintf(file, "\t<filter name=\"%s\" match=\"%s\">\n", escaped, 
+	for (i = 0; i < tabs; i++)
+		fprintf(file, "\t");
+	fprintf(file, "<filter name=\"%s\" match=\"%s\">\n", escaped, 
 		filter->match == SEAUDIT_FILTER_MATCH_ALL? "all" : "any");
 	free(escaped);
 
 	if (filter->desc) {
 		escaped = xmlURIEscapeStr(filter->desc, NULL);
-		fprintf(file, "\t\t<desc>%s</desc>\n", escaped);
+		for (i = 0; i < tabs+1; i++)
+			fprintf(file, "\t");
+		fprintf(file, "<desc>%s</desc>\n", escaped);
 		free(escaped);
 	}
-
-	seaudit_criteria_print(filter->src_type_criteria, file, 2);
-	seaudit_criteria_print(filter->tgt_type_criteria, file, 2);
-	seaudit_criteria_print(filter->src_user_criteria, file, 2);
-	seaudit_criteria_print(filter->tgt_user_criteria, file, 2);
-	seaudit_criteria_print(filter->src_role_criteria, file, 2);
-	seaudit_criteria_print(filter->tgt_role_criteria, file, 2);
-	seaudit_criteria_print(filter->class_criteria, file, 2);
-	seaudit_criteria_print(filter->exe_criteria, file, 2);
-	seaudit_criteria_print(filter->path_criteria, file, 2);
-	seaudit_criteria_print(filter->netif_criteria, file, 2);
-	seaudit_criteria_print(filter->ipaddr_criteria, file, 2);
-	seaudit_criteria_print(filter->ports_criteria, file, 2);
-
-	fprintf(file, "\t</filter>\n</structure>\n");
-	fclose(file);	
-	return 0;
-
+	list = seaudit_filter_get_list(filter);
+	for (node = list->head; node != NULL; node = node->next) {
+		criteria = (seaudit_criteria_t*)node->data;
+		if (criteria)
+			seaudit_criteria_print(criteria, file, tabs+2);
+	}
+	fprintf(file, "\t</filter>\n"); 
 }
 
-int seaudit_filter_load_from_file(seaudit_filter_t **filter, const char *filename)
-{
-	seaudit_filter_parser_data_t parse_data;
-	xmlSAXHandler handler;
-	int err;
-
-	if (!filename)
-		return -1;
-
-	memset(&handler, 0, sizeof(xmlSAXHandler));
-	handler.startElement = my_parse_startElement;
-	handler.endElement = my_parse_endElement;
-	handler.characters = my_parse_characters;
-	memset(&parse_data, 0, sizeof(seaudit_filter_parser_data_t));
-	parse_data.filter = seaudit_filter_create();
-	err = xmlSAXUserParseFile(&handler, &parse_data, filename);
-	seaudit_filter_parser_data_free(&parse_data);
-	if (err)
-		return err;
-	*filter = parse_data.filter;
-	return 0;
-}
 
 
--- setools/libseaudit/filters.h
+++ setools/libseaudit/filters.h
@@ -17,6 +17,8 @@
 #include <libapol/util.h>
 #include "filter_criteria.h"
 
+#define FILTER_FILE_FORMAT_VERSION "1.3"
+
 enum seaudit_filter_match_t {
 	SEAUDIT_FILTER_MATCH_ALL,
 	SEAUDIT_FILTER_MATCH_ANY
@@ -35,6 +37,7 @@ typedef struct seaudit_filter {
 	seaudit_criteria_t *netif_criteria;
 	seaudit_criteria_t *ipaddr_criteria;
 	seaudit_criteria_t *ports_criteria;
+	seaudit_criteria_t *host_criteria;
 	enum seaudit_filter_match_t match;
 	char *name;
 	char *desc;
@@ -49,7 +52,7 @@ void seaudit_filter_set_desc(seaudit_filter_t *seaudit_filter, const char *desc)
 void seaudit_filter_make_dirty_criterias(seaudit_filter_t *seaudit_filter);
 bool_t seaudit_filter_does_message_match(seaudit_filter_t *filter, msg_t *message, audit_log_t *log);
 int seaudit_filter_save_to_file(seaudit_filter_t *filter, const char *filename);
-int seaudit_filter_load_from_file(seaudit_filter_t **filter, const char *filename);
+void seaudit_filter_append_to_file(seaudit_filter_t *filter, FILE *file, int tabs);
 llist_t* seaudit_filter_get_list(seaudit_filter_t *filter);
 
 #endif
