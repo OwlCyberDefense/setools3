af5efca497367926db12d6ca157310e0d80919ec
tresys.com!donp <tresys.com!donp@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-04-07 17:55:59 +0000

    Ported changes from setools-1.2.
    (Logical change 1.162)

--- setools/libseaudit/parse.h
+++ setools/libseaudit/parse.h
@@ -15,23 +15,18 @@
 
 
 #include "auditlog.h"
-#define PARSE_MEMORY_ERROR       -2
-#define PARSE_NO_SELINUX_ERROR   -4
-#define PARSE_SUCCESS             0
-#define PARSE_INVALID_MSG_WARN    2 /* message NOT added to audit log */
-#define PARSE_MALFORMED_MSG_WARN  3 /* message added to audit log anyway */
-#define PARSE_BOTH_MSG_WARN       4 /* invalid and malformed messages found in log */
-#define PARSE_NO_PARSE            5 /* file was eof */
-
+#define	PARSE_RET_SUCCESS		0x00000000	/* success, no warnings nor errors */
+#define PARSE_RET_MEMORY_ERROR		0x00000001	/* general error */
+#define PARSE_RET_EOF_ERROR            	0x00000002	/* file was eof */
+#define PARSE_RET_NO_SELINUX_ERROR   	0x00000004	/* no selinux messages found */
+#define PARSE_RET_INVALID_MSG_WARN	0x00000008	/* invalid message, but added to audit log anyway */
 
 #define PARSE_MEMORY_ERROR_MSG "Memory error while parsing the log!"
 #define PARSE_NO_SELINUX_ERROR_MSG "No SELinux messages found in log!"
 #define PARSE_SUCCESS_MSG "Parse success!"
 #define PARSE_INVALID_MSG_WARN_MSG "Warning! One or more invalid messages found in audit log.  See help file for more information."
-#define PARSE_MALFORMED_MSG_WARN_MSG "Warning! One or more malformed messages found in audit log.  See help file for more information."
-#define PARSE_BOTH_MSG_WARN_MSG "Warning! Invalid and malformed messages found in audit log.  See help file for more information."
 
-int parse_audit(FILE *syslog, audit_log_t *log);
+unsigned int parse_audit(FILE *syslog, audit_log_t *log);
 /* parses the file specified by syslog and puts all selinux audit messages into log 
    it is assumed that log will be created before this function
 */
