fd1d4148e40909be7ab274c1efa7289ccc4750af
tresys.com!donp <tresys.com!donp@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-04-15 19:54:25 +0000

    version text change
    (Logical change 1.198)

--- seuser/seuser_cmd.c
+++ seuser/seuser_cmd.c
@@ -174,7 +174,7 @@ const int seu_show_roles(char **outstr, user_db_t *db, policy_t *policy)
 
 void usage(bool_t brief)
 {
-	fprintf(stdout, "%s (tool ver. %s)\n\n", COPYRIGHT_INFO, SEUSERCMD_VERSION_STRING);
+	fprintf(stdout, "%s (seuser ver. %s)\n", COPYRIGHT_INFO, SEUSERCMD_VERSION_STRING);
 	fprintf(stdout, "\nUsage:\n");
 	fprintf(stdout, "  seuser -X\n");
 	fprintf(stdout, "  seuser delete [-N] username\n");
@@ -320,7 +320,7 @@ int main(int argc, char *argv[])
 	}
 	/* command line */
 	if(strcmp("version", argv[1]) == 0) {
-		fprintf(stdout, "\n%s (tool ver. %s)\n\n", COPYRIGHT_INFO, SEUSERCMD_VERSION_STRING);
+		fprintf(stdout, "\n%s (seuser ver. %s)\n\n", COPYRIGHT_INFO, SEUSERCMD_VERSION_STRING);
 		exit(0);
 	}
 
