55735d392823e4d095bb5f52fc5f3c50bcbb9efa
tresys.com!donp <tresys.com!donp@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-04-30 16:25:18 +0000

    manual merge
    2004/04/30 12:14:37-04:00 tresys.com!donp
    Fixes to enhanced dta analysis.
    
    2004/04/28 11:15:34-04:00 tresys.com!donp
    Change to use generic interfaces for adding object class options to an analysis query.
    
    2004/04/23 17:46:55-04:00 tresys.com!donp
    Changes to support enhanced forward dta.
    
    2004/04/22 17:21:28-04:00 tresys.com!donp
    changes for enhanced dta analysis.
    
    (Logical change 1.218)

--- libapol/analysis.h
+++ libapol/analysis.h
@@ -20,6 +20,23 @@
 #include "util.h"
 
 /*
+ * analysis_obj_options_t allows the inclusion/exclusion of individual permissions
+ * on object classes or entire object classes. 
+ *
+ *	Transitive information flow - if perms is non-NULL then only those 
+ *	permissions are ignored, otherwise the entire object class is ignored. 
+ *
+ *	Forward domain transition - limits the query to select individual permissions 
+ *	on object classes or entire object classes.  
+ *
+ */
+typedef struct analysis_obj_options {
+	int obj_class;   	/* index policy->obj_classes */
+	int num_perms;
+	int *perms;    		/* index of an object class' permission */
+} analysis_obj_options_t;
+
+/*
  * DOMAIN TRANSITION ANALYSIS
  */
 
@@ -57,6 +74,8 @@ typedef struct trans_domain {
 	int	trans_type;		
 	int	num_pt_rules;		/* # of process transition rules */
 	int	*pt_rules;		/* dynamic array of pt rules */
+	int	num_other_rules;	/* # of other transition rules */
+	int	*other_rules;		/* dynamic array of other rules */
 	llist_t *entry_types;		/* list of entrypoint types */
 	bool_t	reverse;		/* reverse direction (0 is non-reverse, anything else is reverse) */
 } trans_domain_t;
@@ -68,13 +87,39 @@ typedef struct domain_trans_analysis {
 	bool_t	reverse;		/* reverse direction (0 is non-reverse, anything else is reverse) */
 } domain_trans_analysis_t;
 
+/*
+ * dta_query_t encapsulates all of the paramaters of a dta query. It should
+ * always be allocated with dta_query_create() and deallocated with
+ * dta_query_destroy(). 
+ *
+ * Limiting by object_types, obj_classes and obj_class permissions is optional.
+ * If the list is empty then no limiting is done. All of the list should contain 
+ * the items that you want to appear in the results. 
+ */
+typedef struct dta_query {
+	int start_type; 		/* index into policy->types */
+	bool_t	reverse;		/* reverse direction (0 is non-reverse, anything else is reverse) */
+	int num_end_types;
+	int *end_types; 		/* indices into policy->types */
+	int num_obj_options; 		/* number of permission options */
+	analysis_obj_options_t *obj_options;
+} dta_query_t;
+
+/*
+ * Information Flow Analysis
+ */
 
 void free_entrypoint_type(void *t);
 void free_trans_domain(void *t);
 void free_domain_trans_analysis(domain_trans_analysis_t *p);
+dta_query_t* dta_query_create(void);
+void dta_query_destroy(dta_query_t *q);
+int dta_query_add_type(dta_query_t *q, int type);
+int dta_query_add_obj_class(dta_query_t *q, int obj_class);
+int dta_query_add_obj_class_perm(dta_query_t *q, int obj_class, int perm);
 domain_trans_analysis_t *new_domain_trans_analysis(void);
 trans_domain_t *new_trans_domain(void); 
-int determine_domain_trans(bool_t reverse, char *start_domain, domain_trans_analysis_t **dta, policy_t *policy);
-
+int determine_domain_trans(dta_query_t *dta_query, 
+			   domain_trans_analysis_t **dta, 
 
 #endif /*_APOLICY_ANALYSIS_H_*/
