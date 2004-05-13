100dbc9f968b3b43c92d87cacd972986874ab39b
Don Patterson <don.patterson@tresys.com>
2004-05-13 20:06:40 +0000

    new function declarations for searching for default policy file. Change to provide default src policy location.
    (Logical change 1.233)

--- setools/libapol/policy-io.h
+++ setools/libapol/policy-io.h
@@ -14,10 +14,21 @@
 
 #include "policy.h"
 
+/* LIBAPOL_DEFAULT_POLICY should be defined in the make environment */
+#ifndef LIBAPOL_DEFAULT_POLICY
+	#define LIBAPOL_DEFAULT_POLICY "/etc/security/selinux/src/policy/policy.conf"
+#endif
+
+#define	SEARCH_BINARY 1
+#define SEARCH_SOURCE 2
+#define SEARCH_BOTH 3
+
 int close_policy(policy_t *policy);
 int open_policy(const char* filename, policy_t **policy);
 int open_partial_policy(const char* filename, unsigned int options, policy_t **policy);
 unsigned int validate_policy_options(unsigned int options);
+int find_default_policy_file(int search_opt, char *policy_file_path);
+const char* decode_find_default_policy_file_err(int err);
 
 #endif   
 
