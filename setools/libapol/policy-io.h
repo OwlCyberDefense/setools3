462af38e6966a43f90e549e28da02dec8ee43e1c
Don Patterson <don.patterson@tresys.com>
2004-05-18 20:16:13 +0000

    Added return codes to header file
    (Logical change 1.248)

--- setools/libapol/policy-io.h
+++ setools/libapol/policy-io.h
@@ -23,12 +23,20 @@
 #define SEARCH_SOURCE 2
 #define SEARCH_BOTH 3
 
+/* * Return codes for find_default_policy_file() function. */
+#define FIND_DEFAULT_SUCCESS 		0
+#define GENERAL_ERROR	 		-1
+#define BIN_POL_FILE_DOES_NOT_EXIST 	-2
+#define SRC_POL_FILE_DOES_NOT_EXIST 	-3
+#define NOT_SELINUX_AWARE		-4
+#define READ_POLICY_VER_FILE_ERROR	-5
+
 int close_policy(policy_t *policy);
 int open_policy(const char* filename, policy_t **policy);
 int open_partial_policy(const char* filename, unsigned int options, policy_t **policy);
 unsigned int validate_policy_options(unsigned int options);
-int find_default_policy_file(int search_opt, char *policy_file_path);
-const char* decode_find_default_policy_file_err(int err);
+int find_default_policy_file(unsigned int search_opt, char **policy_file_path);
+const char* find_default_policy_file_strerr(int err);
 
 #endif   
 
