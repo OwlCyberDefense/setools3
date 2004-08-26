28a05ba28f24f732a2742165dd222efa22373aa6
(none)!tmitchem <(none)!tmitchem@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-08-26 22:49:57 +0000

    Moved a define to fsdata.c More renaming
    2004/07/15 18:25:22-04:00 (none)!tmitchem
    New names
    array of indexes in typeinfo
    strcon and security_context_t replaced with context_t
    
    2004/07/11 18:25:04-04:00 (none)!tmitchem
    
    (Logical change 1.356)

---
+++ setools/libsefs/fsdata.h
@@ -0,0 +1,67 @@
+/* Copyright (C) 2004 Tresys Technology, LLC
+ * see file 'COPYING' for use and warranty information */
+
+/* 
+ * Author: tmitchem@tresys.com 
+ */
+
+/* fsdata.h
+ *
+ * analysis policy database support header 
+ *
+ */
+
+#ifndef _FSDATA_H
+#define _FSDATA_H
+
+/* AVL Tree Handling */
+#include <policy.h>
+#include <avl-util.h>
+
+#ifndef SEFS_XATTR_LABELED_FILESYSTEMS
+#define SEFS_XATTR_LABELED_FILESYSTEMS "ext2 ext3"
+#endif
+
+#ifndef SEFS_XATTR_UNLABELED
+#define SEFS_XATTR_UNLABELED "UNLABELED"
+#endif
+
+typedef struct sefs_fileinfo {
+	struct stat 		sb;
+	unsigned int		numpaths;
+	unsigned int		pathlistsize;
+	context_t 		context;
+	char **			pathnames;
+	char *			link_context;
+} sefs_fileinfo_t;
+
+
+typedef struct sefs_typeinfo {
+	char * 			setypename;
+	unsigned int 		numpaths;
+	unsigned int 		pathlistsize;
+	unsigned int *		pathitems;
+} sefs_typeinfo_t;
+	
+
+typedef struct sefs_filesystem_data {
+	int 			numpaths;
+	int 			numtypes;
+	int			pathlistsize;
+	int			typelistsize;
+	sefs_fileinfo_t *	paths;
+	sefs_typeinfo_t *	types;
+	avl_tree_t		pathtree;
+	avl_tree_t		typetree;
+} sefs_filesystem_data_t;
+
+
+/* Management and creation functions */
+int sefs_filesystem_data_init(sefs_filesystem_data_t * fsd);
+int sefs_filesystem_data_index(sefs_filesystem_data_t * fsd);
+int sefs_scan_tree(char * dir);
+int sefs_filesystem_data_save(sefs_filesystem_data_t * fsd, char * filename);
+int sefs_filesystem_data_load(sefs_filesystem_data_t * fsd, char *filename);
+
+#endif /* _FSDATA_H */
+
