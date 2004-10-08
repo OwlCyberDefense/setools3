1b03eba5c841d0a73f7092dd6733f39a288a39e6
(none)!donp <(none)!donp@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-10-08 19:45:48 +0000

    new prototype for viewing all fields for a selected message within a textbox
    (Logical change 1.382)

--- setools/seaudit/seaudit.h
+++ setools/seaudit/seaudit.h
@@ -72,5 +72,6 @@ void generate_message_header(char *message_header, audit_log_t *audit_log, struc
 void write_avc_message_to_file(FILE *log_file, const avc_msg_t *message, const char *message_header, audit_log_t *audit_log);
 void write_load_policy_message_to_file(FILE *log_file, const load_policy_msg_t *message, const char *message_header);
 void write_boolean_message_to_file(FILE *log_file, const boolean_msg_t *message, const char *message_header, audit_log_t *audit_log);
+void seaudit_window_view_entire_message_in_textbox(GtkTreeView *treeview);
 
 #endif
