771ac2a67632b054e980d505dbb5d8ba42308882
Jeremy Mowery <jmowrey@tresys.com>
2004-10-26 21:07:03 +0000

    changing structures
    (Logical change 1.402)

--- libsefs/fsdata.h
+++ libsefs/fsdata.h
@@ -14,6 +14,8 @@
 #ifndef _FSDATA_H
 #define _FSDATA_H
 
+#include <stdint.h>
+
 /* AVL Tree Handling */
 #include <policy.h>
 #include <avl-util.h>
@@ -26,32 +28,48 @@
 #define SEFS_XATTR_UNLABELED "UNLABELED"
 #endif
 
+typedef enum sefs_classes {
+	NORM_FILE,
+	DIR,
+	LNK_FILE,
+	CHR_FILE,
+	BLK_FILE,
+	SOCK_FILE,
+	FIFO_FILE,
+	ALL_FILES
+} sefs_classes_t;
+
+typedef struct inode_key
+{
+	ino_t			inode;
+	dev_t			device;
+} inode_key_t;
+
 typedef struct sefs_fileinfo {
-	struct stat 		sb;
-	unsigned int		numpaths;
-	unsigned int		pathlistsize;
+	inode_key_t		key;
+	uint32_t		num_links;
 	context_t 		context;
-	char **			pathnames;
+	char **			path_names;
+	char * 			symlink_target;
+	mode_t			mode;
+	sefs_classes_t		obj_class;
 } sefs_fileinfo_t;
 
 
 typedef struct sefs_typeinfo {
-	char * 			setypename;
-	unsigned int 		numpaths;
-	unsigned int 		pathlistsize;
-	unsigned int *		pathitems;
+	char * 			name;
+	uint32_t 		num_inodes;
+	uint32_t *		index_list;
 } sefs_typeinfo_t;
 	
 
 typedef struct sefs_filesystem_data {
-	unsigned int 		numpaths;
-	unsigned int 		numtypes;
-	unsigned int		pathlistsize;
-	unsigned int		typelistsize;
-	sefs_fileinfo_t *	paths;
+	uint32_t 		num_files;
+	uint32_t 		num_types;
+	sefs_fileinfo_t *	files;
 	sefs_typeinfo_t *	types;
-	avl_tree_t		pathtree;
-	avl_tree_t		typetree;
+	avl_tree_t		file_tree;
+	avl_tree_t		type_tree;
 } sefs_filesystem_data_t;
 
 
@@ -61,6 +79,7 @@ int sefs_filesystem_data_index(sefs_filesystem_data_t * fsd);
 int sefs_scan_tree(char * dir);
 int sefs_filesystem_data_save(sefs_filesystem_data_t * fsd, char * filename);
 int sefs_filesystem_data_load(sefs_filesystem_data_t * fsd, char *filename);
+int sefs_get_file_class(const struct stat *statptr);
 
 #endif /* _FSDATA_H */
 
