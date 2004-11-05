fb158fb8cd2f9fc7a1f973200660c2da628255e5
(none)!donp <(none)!donp@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-11-05 21:38:40 +0000

    Change to use a more generic way of stripping tcl list of empty items, so loading queries will run in tcl8.3.
    (Logical change 1.453)

--- setools/apol/dta_module.tcl
+++ setools/apol/dta_module.tcl
@@ -1352,12 +1352,9 @@ proc Apol_Analysis_dta::load_query_options { file_channel parentDlg } {
 	# by a single space. Then split this string into a list using space and colon characters
 	# as the delimeters.	
 	set query_options_tmp [split [join $query_options_tmp " "] " :"]
-	set indices [lsearch -all $query_options_tmp ""]
-	set length [llength $query_options_tmp]
-	for {set i 0} {$i < $length} {incr i} {
-		if {[lsearch -exact -integer $indices $i] == -1} {
-			set query_options [lappend query_options [lindex $query_options_tmp $i]]
-		}	
+	set query_options [ApolTop::strip_list_of_empty_items $query_options_tmp]
+	if {$query_options == ""} {
+		return -code error "No query parameters were found."
 	}
 	
      	if {[lindex $query_options 0] != "\{\}"} {
