5a29a8fb9a122ea0913f33f24a44be67646fb6cc
Don Patterson <don.patterson@tresys.com>
2004-11-10 01:44:42 +0000

    minor change to error code name.
    (Logical change 1.455)

--- libapol/policy-io.h
+++ libapol/policy-io.h
@@ -20,13 +20,13 @@
 #endif
 
 /* * Return codes for find_default_policy_file() function. */
-#define FIND_DEFAULT_SUCCESS 		0
-#define GENERAL_ERROR	 		-1
-#define BIN_POL_FILE_DOES_NOT_EXIST 	-2
-#define SRC_POL_FILE_DOES_NOT_EXIST 	-3
-#define BOTH_POL_FILE_DO_NOT_EXIST 	-4
-#define NOT_SELINUX_AWARE		-5
-#define INVALID_SEARCH_OPTIONS		-6
+#define FIND_DEFAULT_SUCCESS 			0
+#define GENERAL_ERROR	 			-1
+#define BIN_POL_FILE_DOES_NOT_EXIST 		-2
+#define SRC_POL_FILE_DOES_NOT_EXIST 		-3
+#define BOTH_POL_FILE_DO_NOT_EXIST 		-4
+#define POLICY_INSTALL_DIR_DOES_NOT_EXIST	-5
+#define INVALID_SEARCH_OPTIONS			-6
 
 int close_policy(policy_t *policy);
 int open_policy(const char* filename, policy_t **policy);
