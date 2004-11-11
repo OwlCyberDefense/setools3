0432023b6044ba7667a954632ea1cc954f3e57c1
Don Patterson <don.patterson@tresys.com>
2004-11-11 04:51:42 +0000

    Fix to tcl/tk application bug. Bad variable name.
    (Logical change 1.458)

--- setools/apol/fulflow_module.tcl
+++ setools/apol/fulflow_module.tcl
@@ -2814,12 +2814,12 @@ proc Apol_Analysis_fulflow::advanced_filters_create_dialog {path_name title_txt}
 	bind ${bind_tag_id}_lbox_excl_Tag <<ListboxSelect>> "focus -force $f_opts($path_name,lbox_excl)"
 	
 	bind ${bind_tag_id}_lbox_incl_Tag <KeyPress> {ApolTop::tklistbox_select_on_key_callback \
-			$Apol_Analysis_fulflow::lbox_incl \
-			$Apol_Analysis_fulflow::filtered_incl_types \
+			$Apol_Analysis_fulflow::f_opts($path_name,lbox_incl) \
+			Apol_Analysis_fulflow::f_opts($path_name,filtered_incl_types) \
 			%K}
 	bind ${bind_tag_id}_lbox_excl_Tag <KeyPress> {ApolTop::tklistbox_select_on_key_callback \
-			$Apol_Analysis_fulflow::lbox_excl \
-			$Apol_Analysis_fulflow::filtered_excl_types \
+			$Apol_Analysis_fulflow::f_opts($path_name,lbox_excl) \
+			Apol_Analysis_fulflow::f_opts($path_name,filtered_excl_types) \
 			%K}
 			
         set include_bttn [Button $middle_f.include_bttn -text "<--" \
