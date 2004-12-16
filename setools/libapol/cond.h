48a94c5646e0e28545f8a90a1272c7a8edb67ab0
(none)!kmacmillan <(none)!kmacmillan@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2004-12-16 20:38:46 +0000

    merge.
    (Logical change 1.474)

--- setools/libapol/cond.h
+++ setools/libapol/cond.h
@@ -21,6 +21,25 @@
 #define COND_NEQ	7 /* bool != bool */
 #define COND_LAST	8
 
+/* In theory we can handle 31 bools with this code.  In 
+   practice I get a malloc failure for the space required
+   to hold the results for 31 bools, so we can only do
+   30 bools or less.
+
+   if we want to handle more than 31 bools we need to 
+   use something bigger than an unsigned long to loop
+   thru all the possible bit/bool combinations.
+   Note that it takes ~1.3 minutes to loop thru all 2^31 
+   bit combos _without_ doing any serious computation     
+   on an idle 2.8P4 with 1G memory compiled with -O3  
+ 
+   ALSO, we need +500MB to store results for one 31 bool expr
+   (2^31)/8 == 536,870,912 (512MB)
+   (2^30)/8 == 134,217,728 (128MB)
+   
+   So we will set the limit at a reasonable size of 25 booleans
+   per expression; more than that will always return false. */
+#define COND_MAX_BOOLS 25
 #define COND_EXPR_MAXDEPTH 10
 
 /* policy DB structures */
@@ -56,6 +75,7 @@ typedef struct cond_rule_list {
 typedef struct cond_expr_item {
 	bool_t cur_state;		/* the current logical result of the expression */
 	cond_expr_t *expr;
+	int	num_bools;		/* # of booleans in the expressions; a heuristic for compare */
 	cond_rule_list_t *true_list;
 	cond_rule_list_t *false_list;
 } cond_expr_item_t;
@@ -70,5 +90,6 @@ int cond_free_rules_list(cond_rule_list_t *rl);
 int cond_free_expr_item(cond_expr_item_t *c);
 int cond_evaluate_expr(cond_expr_t *expr, struct policy *policy);
 bool_t cond_exprs_equal(cond_expr_t *a, cond_expr_t *b);
+bool_t cond_exprs_semantic_equal(cond_expr_t *a, cond_expr_t *b, struct policy *p, bool_t *inverse);
 
 #endif
