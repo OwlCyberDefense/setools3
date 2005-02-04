b9f33517eaa94af032b045241576f2d0dd9ad348
(none)!method <(none)!method@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2005-02-04 15:23:01 +0000

    copyright
    (Logical change 1.592)

--- seaudit/report.c
+++ seaudit/report.c
@@ -1,4 +1,4 @@
-/* Copyright (C) 2004 Tresys Technology, LLC
+/* Copyright (C) 2004-2005 Tresys Technology, LLC
  * see file 'COPYING' for use and warranty information 
  * 
  * Author: Don Patterson <don.patterson@tresys.com>
