58e6dfc40376c93085ec799cac6c82abc200cf5b
Don Patterson <don.patterson@tresys.com>
2005-02-08 14:31:42 +0000

    Updated copyright statement.
    (Logical change 1.607)

--- setools/apol/cond_rules_tab.tcl
+++ setools/apol/cond_rules_tab.tcl
@@ -1,4 +1,4 @@
-# Copyright (C) 2004 Tresys Technology, LLC
+# Copyright (C) 2004-2005 Tresys Technology, LLC
 # see file 'COPYING' for use and warranty information 
 
 # TCL/TK GUI for SE Linux policy analysis
