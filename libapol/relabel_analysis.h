c76d3a477a855cece982e41d10fb3056544549fe
(none)!mowery <(none)!mowery@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2005-03-02 13:47:37 +0000

    start flag to fix printing in obj mode
    2005/02/25 21:35:41-05:00 (none)!mowery
    whitespace
    
    2005/02/25 17:28:48-05:00 (none)!mowery
    foo
    
    (Logical change 1.743)

--- libapol/relabel_analysis.h
+++ libapol/relabel_analysis.h
@@ -1,88 +1,66 @@
-/* Copyright (C) 2004-2005 Tresys Technology, LLC
+/* Copyright (C) 2005 Tresys Technology, LLC
  * see file 'COPYING' for use and warranty information */
 
 /* 
  * Author: Jeremy A. Mowery jmowery@tresys.com
  */
 
-#include <policy.h>
-#include <policy-io.h>
-#include <policy-query.h>
+#ifndef _RELABEL_ANALYSIS_H_
+#define _RELABEL_ANALYSIS_H_
 
-#ifndef __RELABEL_ANALYSIS_H_FILE__
-#define __RELABEL_ANALYSIS_H_FILE__
+#include "policy.h"
 
-/* internal error code defines */
-#define NOTHERE -6 /* could not find requested element */
+/* defines for mode */
+#define AP_RELABEL_MODE_OBJ	0x01
+#define AP_RELABEL_MODE_SUBJ	0x02
 
-/* list choice codes */
-#define NOLIST   0
-#define TOLIST   1
-#define FROMLIST 2
-#define BOTHLIST 3
-#define ANYLIST  4
+/* defines for direction flag */
+#define AP_RELABEL_DIR_NONE	0x00
+#define AP_RELABEL_DIR_TO	0x01
+#define AP_RELABEL_DIR_FROM	0x02
+#define AP_RELABEL_DIR_BOTH	(AP_RELABEL_DIR_TO|AP_RELABEL_DIR_FROM)
+#define AP_RELABEL_DIR_START	0x04
 
-/* name of relabeling permissions */
-#define RELABELTO "relabelto"
-#define RELABELFROM "relabelfrom"
 
-/* search mode definitions */
-#define MODE_TO   1
-#define MODE_FROM 2
-#define MODE_BOTH 3
-#define MODE_DOM  4
+/* data structures */
+typedef struct ap_relabel_rule {
+	int		rule_index;
+	unsigned char 	direction;
+} ap_relabel_rule_t;
 
-typedef struct type_obj {
-	int type;
-	obj_perm_set_t *perm_sets;
-	int num_perm_sets;
-	int *rules;
-	int num_rules;
-	int list;
-} type_obj_t;
+typedef struct ap_relabel_subject {
+	int			source_type;
+	ap_relabel_rule_t	*rules;
+	int			num_rules;
+	unsigned char 		direction;
+} ap_relabel_subject_t;
 
-typedef struct relabel_set {
-	int subject_type;
-	type_obj_t *types;
-	int num_types;
-} relabel_set_t;
+typedef struct ap_relabel_object {
+	int			object_class;
+	ap_relabel_subject_t 	*subjects;
+	int			num_subjects;
+	unsigned char		direction;
+} ap_relabel_object_t;
 
-typedef struct relabel_mode {
-	int mode; 
-	bool_t filter;
-	bool_t transitive;
-	unsigned int trans_steps;
-} relabel_mode_t;
+typedef struct ap_relabel_target {
+	int			target_type;
+	ap_relabel_object_t	*objects;
+	int			num_objects;
+	unsigned char		direction;
+} ap_relabel_target_t;
 
-typedef struct relabel_result {
-	int *types;
-	int num_types;
-	int **subjects;
-	int *num_subjects; /* num_types is size */
-	relabel_mode_t *mode;
-	relabel_set_t *set;
-} relabel_result_t;
+typedef struct ap_relabel_result {
+	int			start_type;
+	unsigned char 		mode;
+	unsigned char		requested_direction;
+	ap_relabel_target_t	*targets;
+	int			num_targets;
+} ap_relabel_result_t;
 
-typedef struct relabel_filter {
-	obj_perm_set_t *perm_sets; /* Anything in here is what is displayed. */
-	int num_perm_sets;
-} relabel_filter_t;
+/* query function */
+int ap_relabel_query(int start_type, unsigned char mode, unsigned char direction, ap_relabel_result_t *res, policy_t *policy);
 
-int apol_type_obj_init(type_obj_t *obj);
-int apol_relabel_set_init(relabel_set_t *set);
-int apol_relabel_result_init(relabel_result_t *res);
-int apol_relabel_mode_init(relabel_mode_t *mode);
-int apol_relabel_filter_init(relabel_filter_t *fltr);
+/* clean-up function */
+void ap_relabel_result_destroy(ap_relabel_result_t *res);
 
-void apol_free_type_obj_data(type_obj_t *obj);
-void apol_free_relabel_set_data(relabel_set_t *set);
-void apol_free_relabel_result_data(relabel_result_t *res);
-void apol_free_relabel_filter_data(relabel_filter_t *fltr);
-
-int apol_where_is_type_in_list(relabel_set_t *set, int type, int list);
-
-int apol_fill_filter_set (char *object_class, char *permission, relabel_filter_t *filter, policy_t *policy);
-int apol_do_relabel_analysis(relabel_set_t **sets, policy_t *policy);
-int apol_query_relabel_analysis(relabel_set_t *sets, int type, relabel_result_t *res, policy_t *policy, relabel_mode_t *mode, relabel_filter_t *filter);
-
-#endif /* __RELABEL_ANALYSIS_H_FILE__ */
+#endif
