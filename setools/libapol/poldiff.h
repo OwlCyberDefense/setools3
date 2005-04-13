82419d0a78d485282a764168c1ca71d3a9c4743c
Brandon Whalen <bwhalen@tresys.com>
2005-04-13 19:15:14 +0000

    patch
    (Logical change 1.792)

--- setools/libapol/poldiff.h
+++ setools/libapol/poldiff.h
@@ -100,8 +100,8 @@ void apol_free_diff_result(bool_t close_pols, apol_diff_result_t *adr);
 apol_diff_result_t *apol_diff_policies(unsigned int opts, policy_t *p1, policy_t *p2);
 int make_p2_key(avh_key_t *p1key, avh_key_t *p2key, policy_t *p1, policy_t *p2);
 bool_t does_cond_match(avh_node_t *n1, policy_t *p1, avh_node_t *n2, policy_t *p2, bool_t *inverse);
-ap_cond_expr_diff_t *find_cdiff_in_policy(ap_cond_expr_diff_t *cond_expr_diff,apol_diff_t *diff2,policy_t *p1,policy_t *p2);
-int find_cond_in_policy(int p1_idx,policy_t *p1,policy_t *p2);
+ap_cond_expr_diff_t *find_cdiff_in_policy(ap_cond_expr_diff_t *cond_expr_diff,apol_diff_t *diff2,policy_t *p1,policy_t *p2,bool_t *inverse);
+int find_cond_in_policy(int p1_idx,policy_t *p1,policy_t *p2,bool_t noinverse);
 #endif /* _APOLICY_POLDIFF_H_ */
 
 
