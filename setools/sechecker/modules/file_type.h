3048689a32726ccbd49f31eabca1399346d70874
Jeremy Mowery <jmowery@tresys.com>
2005-07-07 19:55:06 +0000

    updated function names

--- setools/sechecker/modules/file_type.h
+++ setools/sechecker/modules/file_type.h
@@ -20,10 +20,10 @@ int file_type_register(sechk_lib_t *lib);
 int file_type_init(sechk_module_t *mod, policy_t *policy);
 int file_type_run(sechk_module_t *mod, policy_t *policy);
 void file_type_free(sechk_module_t *mod);
-char *file_type_get_output_str(sechk_module_t *mod, policy_t *policy);
+int file_type_print_output(sechk_module_t *mod, policy_t *policy);
 sechk_result_t *file_type_get_result(sechk_module_t *mod);
  
 int file_type_get_file_type_list(sechk_module_t *mod, int **array, int *size);
 
-file_type_data_t *new_file_type_data(void);
-void free_file_type_data(file_type_data_t **datum);
+file_type_data_t *file_type_data_new(void);
+void file_type_data_free(file_type_data_t *datum);
