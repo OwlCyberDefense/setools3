eb3e319a927328eb0a206219269a4887faa12aab
jmowery <jmowery@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2005-07-07 19:55:06 +0000

    updated function names

--- sechecker/modules/domain_and_file_type.h
+++ sechecker/modules/domain_and_file_type.h
@@ -23,10 +23,8 @@ int domain_and_file_type_register(sechk_lib_t *lib);
 int domain_and_file_type_init(sechk_module_t *mod, policy_t *policy);
 int domain_and_file_type_run(sechk_module_t *mod, policy_t *policy);
 void domain_and_file_type_free(sechk_module_t *mod);
-char *domain_and_file_type_get_output_str(sechk_module_t *mod, policy_t *policy);
+int domain_and_file_type_print_output(sechk_module_t *mod, policy_t *policy);
 sechk_result_t *domain_and_file_type_get_result(sechk_module_t *mod);
- 
-/* TODO: declare any other functions needed by get_module_function */
 
-domain_and_file_type_data_t *new_domain_and_file_type_data(void);
-void free_domain_and_file_type_data(domain_and_file_type_data_t **datum);
+domain_and_file_type_data_t *domain_and_file_type_data_new(void);
+void domain_and_file_type_data_free(domain_and_file_type_data_t *datum);
