df4ffba14a946fd7411cdb35f04c8043f936729f
kcarr <kcarr@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2005-07-14 14:55:16 +0000

    jeremys patch for fixing sechecker design

--- setools/sechecker/modules/file_type.h
+++ setools/sechecker/modules/file_type.h
@@ -10,20 +10,18 @@
 #include "policy.h"
 
 typedef struct file_type_data {
-	char		*mod_header;
 	int		*file_type_attribs;
 	int		num_file_type_attribs;
-	unsigned char	outformat;
 } file_type_data_t;
 
 int file_type_register(sechk_lib_t *lib);
 int file_type_init(sechk_module_t *mod, policy_t *policy);
 int file_type_run(sechk_module_t *mod, policy_t *policy);
-void file_type_free(sechk_module_t *mod);
+void file_type_data_free(sechk_module_t *mod);
 int file_type_print_output(sechk_module_t *mod, policy_t *policy);
 sechk_result_t *file_type_get_result(sechk_module_t *mod);
  
 int file_type_get_file_type_list(sechk_module_t *mod, int **array, int *size);
 
 file_type_data_t *file_type_data_new(void);
-void file_type_data_free(file_type_data_t *datum);
+
--- setools/sechecker/register_list.h
+++ setools/sechecker/register_list.h
@@ -11,16 +11,16 @@
 
 #include "sechecker.h"
 
-/* TODO: to add a module declare it's register function as 
+/* TODO: to add a module declare it's register function as
  * extern int <module_name>_register(sechk_lib_t *lib);
- * here and add it's address to the array at the bottom 
+ * here and add it's address to the array in register_list.c
  * don't forget to add any necessary options to the config file */
 
 /* extern register functions declarations */
 extern int domain_type_register(sechk_lib_t *lib);
 extern int file_type_register(sechk_lib_t *lib);
 extern int domain_and_file_type_register(sechk_lib_t *lib);
-/* additional externs go here ... */
+/* TODO: additional externs go here ... */
 
 int sechk_register_get_num_modules();
 
