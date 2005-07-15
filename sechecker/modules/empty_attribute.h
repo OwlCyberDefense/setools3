9bb5b3b527b872681d54ec714d6d565282908331
Kevin Carr <kcarr@tresys.com>
2005-07-15 19:41:02 +0000

    sechecker changes to compile in remaining modules

--- sechecker/modules/empty_attribute.h
+++ sechecker/modules/empty_attribute.h
@@ -10,16 +10,17 @@
 #include "policy.h"
 
 typedef struct empty_attribute_data {
-	char		*mod_header;
-	unsigned char	outformat;
 } empty_attribute_data_t;
 
 int empty_attribute_register(sechk_lib_t *lib);
 int empty_attribute_init(sechk_module_t *mod, policy_t *policy);
 int empty_attribute_run(sechk_module_t *mod, policy_t *policy);
 void empty_attribute_free(sechk_module_t *mod);
-char *empty_attribute_get_output_str(sechk_module_t *mod, policy_t *policy);
+int empty_attribute_print_output(sechk_module_t *mod, policy_t *policy);
 sechk_result_t *empty_attribute_get_result(sechk_module_t *mod);
- 
-empty_attribute_data_t *new_empty_attribute_data(void);
-void free_empty_attribute_data(empty_attribute_data_t **datum);
+
+
+/* The following function is used to allocate and initialize
+ * the private data storage structure for this module */
+empty_attribute_data_t *empty_attribute_data_new(void);
+
--- sechecker/modules/empty_role.h
+++ sechecker/modules/empty_role.h
@@ -9,17 +9,19 @@
 #include "sechecker.h"
 #include "policy.h"
 
+/* The empty_role_data structure is used to hold the check specific
+ *  private data of a module. */
 typedef struct empty_role_data {
-	char		*mod_header;
-	unsigned char	outformat;
 } empty_role_data_t;
 
 int empty_role_register(sechk_lib_t *lib);
 int empty_role_init(sechk_module_t *mod, policy_t *policy);
 int empty_role_run(sechk_module_t *mod, policy_t *policy);
 void empty_role_free(sechk_module_t *mod);
-char *empty_role_get_output_str(sechk_module_t *mod, policy_t *policy);
+int empty_role_print_output(sechk_module_t *mod, policy_t *policy);
 sechk_result_t *empty_role_get_result(sechk_module_t *mod);
 
-empty_role_data_t *new_empty_role_data(void);
-void free_empty_role_data(empty_role_data_t **datum);
+/* The following function is used to allocate and initialize
+ * the private data storage structure for this module */
+empty_role_data_t *empty_role_data_new(void);
+
