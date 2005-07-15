96397170ea75a133284dbae115fc8de389f396af
Kevin Carr <kcarr@tresys.com>
2005-07-15 19:41:02 +0000

    sechecker changes to compile in remaining modules

--- setools/sechecker/register_list.h
+++ setools/sechecker/register_list.h
@@ -20,8 +20,13 @@
 extern int domain_type_register(sechk_lib_t *lib);
 extern int file_type_register(sechk_lib_t *lib);
 extern int domain_and_file_type_register(sechk_lib_t *lib);
+extern int empty_attribute_register(sechk_lib_t *lib);
+extern int empty_role_register(sechk_lib_t *lib);
+extern int empty_user_register(sechk_lib_t *lib);
+extern int unused_role_register(sechk_lib_t *lib);
+extern int unused_type_register(sechk_lib_t *lib);
 /* TODO: additional externs go here ... */
 
-int sechk_register_get_num_modules();
+int sechk_register_list_get_num_modules();
 
 #endif /* SECHK_REGISTER_LIST_H */
