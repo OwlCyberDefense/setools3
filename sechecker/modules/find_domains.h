73083f52fac8a225c57a4580b6e31aa9cb537974
Jeremy Mowery <jmowery@tresys.com>
2005-07-19 20:17:24 +0000

    fixes missed in first two rename module updates - This line, and those below, will be ignored--
    M    sechecker/register_list.c
    M    sechecker/register_list.h
    M    sechecker/modules/find_domains.h
    M    sechecker/modules/find_domains.c

--- sechecker/modules/find_domains.h
+++ sechecker/modules/find_domains.h
@@ -9,18 +9,18 @@
 #include "sechecker.h"
 #include "policy.h"
 
-typedef struct find_domians_data {
+typedef struct find_domains_data {
 	int		*domain_attribs;
 	int		num_domain_attribs;
-} find_domians_data_t;
+} find_domains_data_t;
 
-int find_domians_register(sechk_lib_t *lib);
-int find_domians_init(sechk_module_t *mod, policy_t *policy);
-int find_domians_run(sechk_module_t *mod, policy_t *policy);
-void find_domians_data_free(sechk_module_t *mod);
-int find_domians_print_output(sechk_module_t *mod, policy_t *policy);
-sechk_result_t *find_domians_get_result(sechk_module_t *mod);
+int find_domains_register(sechk_lib_t *lib);
+int find_domains_init(sechk_module_t *mod, policy_t *policy);
+int find_domains_run(sechk_module_t *mod, policy_t *policy);
+void find_domains_data_free(sechk_module_t *mod);
+int find_domains_print_output(sechk_module_t *mod, policy_t *policy);
+sechk_result_t *find_domains_get_result(sechk_module_t *mod);
  
-int find_domians_get_list(sechk_module_t *mod, int **array, int *size);
+int find_domains_get_list(sechk_module_t *mod, int **array, int *size);
 
-find_domians_data_t *find_domians_data_new(void);
+find_domains_data_t *find_domains_data_new(void);
