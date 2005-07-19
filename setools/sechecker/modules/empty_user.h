e1a6a52929f4a28320da560dab2a0e34dfa4af9b
Jeremy Mowery <jmowery@tresys.com>
2005-07-19 19:53:35 +0000

    modifying internal names before renaming files next update will rename the files themselves

--- setools/sechecker/modules/empty_user.h
+++ setools/sechecker/modules/empty_user.h
@@ -9,19 +9,19 @@
 #include "sechecker.h"
 #include "policy.h"
 
-/* The empty_user_data structure is used to hold the check specific
+/* The users_wo_roles_data structure is used to hold the check specific
  *  private data of a module. */
-typedef struct empty_user_data {
-} empty_user_data_t;
+typedef struct users_wo_roles_data {
+} users_wo_roles_data_t;
 
-int empty_user_register(sechk_lib_t *lib);
-int empty_user_init(sechk_module_t *mod, policy_t *policy);
-int empty_user_run(sechk_module_t *mod, policy_t *policy);
-void empty_user_free(sechk_module_t *mod);
-int empty_user_print_output(sechk_module_t *mod, policy_t *policy);
-sechk_result_t *empty_user_get_result(sechk_module_t *mod);
+int users_wo_roles_register(sechk_lib_t *lib);
+int users_wo_roles_init(sechk_module_t *mod, policy_t *policy);
+int users_wo_roles_run(sechk_module_t *mod, policy_t *policy);
+void users_wo_roles_free(sechk_module_t *mod);
+int users_wo_roles_print_output(sechk_module_t *mod, policy_t *policy);
+sechk_result_t *users_wo_roles_get_result(sechk_module_t *mod);
 
 /* The following function is used to allocate and initialize
  * the private data storage structure for this module */
-empty_user_data_t *empty_user_data_new(void);
+users_wo_roles_data_t *users_wo_roles_data_new(void);
 
