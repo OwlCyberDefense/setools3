29923587bbe8ea6583ab91602ded6175b182fa5d
kcarr <kcarr@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2005-08-04 19:28:39 +0000

    sechecker changes - support for profiles etc.  and updated Makefiles

--- setools/sechecker/modules/roles_not_in_allow.c
+++ setools/sechecker/modules/roles_not_in_allow.c
@@ -135,14 +135,11 @@ int roles_not_in_allow_register(sechk_lib_t *lib)
 
 /* The init function creates the module's private data storage object
  * and initializes its values based on the options parsed in the config
- * file. It also checks that the requirements and dependencies are met.
- * This function also defines the module header, which provides a brief
- * explanation of the check performed by the module. */
+ * file. */
 int roles_not_in_allow_init(sechk_module_t *mod, policy_t *policy)
 {
 	sechk_name_value_t *opt = NULL;
 	roles_not_in_allow_data_t *datum = NULL;
-	bool_t test = FALSE;
 
 	if (!mod || !policy) {
 		fprintf(stderr, "Error: invalid parameters\n");
@@ -159,26 +156,6 @@ int roles_not_in_allow_init(sechk_module_t *mod, policy_t *policy)
 		return -1;
 	}
 	mod->data = datum;
-
-	opt = mod->requirements;
-	while (opt) {
-		test = FALSE;
-		test = sechk_lib_check_requirement(opt, library);
-		if (!test) {
-			return -1;
-		}
-		opt = opt->next;
-	}
-
-	opt = mod->dependencies;
-	while (opt) {
-		test = FALSE;
-		test = sechk_lib_check_dependency(opt, library);
-		if (!test) {
-			return -1;
-		}
-		opt = opt->next;
-	}
 
 	opt = mod->options;
 	while (opt) {
@@ -407,7 +384,7 @@ int roles_not_in_allow_print_output(sechk_module_t *mod, policy_t *policy)
 	if (!outformat)
 		return 0; /* not an error - no output is requested */
 
-	printf("Module: %s\n", mod_name);
+	printf("\nModule: %s\n", mod_name);
 	/* print the header */
 	if (outformat & SECHK_OUT_HEADER) {
 		printf("%s\n\n", mod->header);
--- setools/sechecker/modules/users_wo_roles.c
+++ setools/sechecker/modules/users_wo_roles.c
@@ -121,14 +121,11 @@ int users_wo_roles_register(sechk_lib_t *lib)
 
 /* The init function creates the module's private data storage object
  * and initializes its values based on the options parsed in the config
- * file. It also checks that the requirements and dependencies are met.
- * This function also defines the module header, which provides a brief
- * explanation of the check performed by the module. */
+ * file. */
 int users_wo_roles_init(sechk_module_t *mod, policy_t *policy)
 {
 	sechk_name_value_t *opt = NULL;
 	users_wo_roles_data_t *datum = NULL;
-	bool_t test = FALSE;
 
 	if (!mod || !policy) {
 		fprintf(stderr, "Error: invalid parameters\n");
@@ -145,26 +142,6 @@ int users_wo_roles_init(sechk_module_t *mod, policy_t *policy)
 		return -1;
 	}
 	mod->data = datum;
-
-	opt = mod->requirements;
-	while (opt) {
-		test = FALSE;
-		test = sechk_lib_check_requirement(opt, library);
-		if (!test) {
-			return -1;
-		}
-		opt = opt->next;
-	}
-
-	opt = mod->dependencies;
-	while (opt) {
-		test = FALSE;
-		test = sechk_lib_check_dependency(opt, library);
-		if (!test) {
-			return -1;
-		}
-		opt = opt->next;
-	}
 
 	opt = mod->options;
 	while (opt) {
@@ -310,7 +287,7 @@ int users_wo_roles_print_output(sechk_module_t *mod, policy_t *policy)
 		return -1;
 	}
 
-	printf("Module: %s\n", mod_name);
+	printf("\nModule: %s\n", mod_name);
 	/* print the header */
 	if (outformat & SECHK_OUT_HEADER) {
 		printf("%s\n\n", mod->header);
