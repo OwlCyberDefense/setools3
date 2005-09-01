32defb73fe9ef1d50de72d237a245dbb473eb177
Kevin Carr <kcarr@tresys.com>
2005-09-01 16:36:53 +0000

    added icons back to menuitems, added copy menuitem

--- setools/sediff/sediff_gui.h
+++ setools/sediff/sediff_gui.h
@@ -26,40 +26,43 @@
 #define FIND_FORWARD_ID      "sediff_find_forward"
 #define FIND_ENTRY_ID        "sediff_find_text_entry"
 
+/* STRUCT: sediff_app_t
+   This structure is used to control the gui.  It contains the links
+   to all necessary buffers, textviews, dlgs, etc that are needed.  */
 typedef struct sediff_app {
-	GtkWindow *window;		/* the main window */
-	GtkWindow *open_dlg; 
-	GtkWidget *modal_dlg;
-	GtkWidget *dummy_view;
-	GladeXML *window_xml;
-	GladeXML *open_dlg_xml;
-	GtkWidget *tree_view;	
-	GtkTextBuffer *policy1_text;
-	GtkTextBuffer *policy2_text;
-	GList *callbacks;
+	GtkWindow *window;		  /* the main window */
+	GtkWindow *open_dlg;              /* dialog box used when opening up the policies */
+	GtkWidget *modal_dlg;             /* a modal dlg used for keeping user input from happening on long computations */
+	GtkWidget *dummy_view;            /* this is a view we put in the left hand pane when we have no diff, and therefore no treeview */
+	GladeXML *window_xml;             /* the main windows xml */
+	GladeXML *open_dlg_xml;           /* the open dialogs xml */
+	GtkWidget *tree_view;	          /* the treeview seen on left hand pane */
+	GtkTextBuffer *policy1_text;      /* the text buffer we stored policy 1 in */
+	GtkTextBuffer *policy2_text;      /* the text buffer we store policy 2 in */
+	GList *callbacks;               
 	gint progress_completed;
-	GtkTextBuffer *main_buffer;
-	GtkTextBuffer *te_buffer;
-	GtkTextBuffer *te_add_buffer;
-	GtkTextBuffer *te_rem_buffer;
-	GtkTextBuffer *te_chg_buffer;
-	GtkTextBuffer *te_add_type_buffer;
-	GtkTextBuffer *te_rem_type_buffer;
-	GtkTextBuffer *summary_buffer;
-	GtkTextBuffer *cond_buffer;
-	GtkTextBuffer *cond_add_buffer;
-	GtkTextBuffer *cond_rem_buffer;
-	GtkTextBuffer *cond_chg_buffer;
-	GString *p1_filename;
-	GString *p2_filename;
-	ap_single_view_diff_t *svd;
-	apol_diff_result_t *diff_results;
-	struct sediff_rename_types *rename_types_window;
-	struct sediff_find_window *find_window;
-	policy_t *p1;
-	policy_t *p2;
+	GtkTextBuffer *main_buffer;       /* the generic buffer used for everything but te rules and conditionals(because they take so long to draw */
+	GtkTextBuffer *te_buffer;         /* the top level node in the te diff */
+	GtkTextBuffer *te_add_buffer;     /* the added te rules buffer */
+	GtkTextBuffer *te_rem_buffer;     /* the removed te rules buffer */
+	GtkTextBuffer *te_chg_buffer;     /* the changed te rules buffer */
+	GtkTextBuffer *te_add_type_buffer;/* the te rules added because of a new type buffer */
+	GtkTextBuffer *te_rem_type_buffer;/* the te rules removed because of a missing type buffer */
+	GtkTextBuffer *summary_buffer;    /* the summary buffer */
+	GtkTextBuffer *cond_buffer;       /* the top level conditional buffer */
+	GtkTextBuffer *cond_add_buffer;   /* the added conditionals buffer */
+	GtkTextBuffer *cond_rem_buffer;   /* the removed conditionals buffer */
+	GtkTextBuffer *cond_chg_buffer;   /* the changed conditionals buffer */
+	GString *p1_filename;             /* the name of policy 1 */
+	GString *p2_filename;             /* the name of policy 2 */
+	ap_single_view_diff_t *svd;       /* the single view diff struct */
+	struct sediff_rename_types *rename_types_window; /* the renamed types window reference */
+	struct sediff_find_window *find_window;          /* the find window reference */
+	policy_t *p1;                     /* the policy 1 struct */
+	policy_t *p2;                     /* the policy 2 struct */
 } sediff_app_t;
 
+/* return the textview currently displayed to the user */
 GtkTextView *sediff_get_current_view(sediff_app_t *app);
 
 #endif
