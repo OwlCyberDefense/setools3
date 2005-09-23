f60c1f73dcf50520caef2d851fe713d57403a8ab
Kevin Carr <kcarr@tresys.com>
2005-09-23 15:52:26 +0000

    sechecker profile fixes

--- setools/sechecker/register_list.c
+++ setools/sechecker/register_list.c
@@ -10,20 +10,22 @@
 
 static int sechk_register_num_modules=-1;
 
-/* array of module names and register functions*/
+/* NULL terminated array of module names and register functions */
 sechk_module_name_reg_t sechk_register_list[] = { 
-	{"find_domains", &find_domains_register},
-	{"find_file_types", &find_file_types_register},
-	{"domain_and_file_type",&domain_and_file_type_register},
-	{"attributes_wo_types_register",&attributes_wo_types_register},
-	{"roles_wo_types",&roles_wo_types_register},
-	{"users_wo_roles",&users_wo_roles_register},
-	{"roles_not_in_allow",&roles_not_in_allow_register},
-	{"types_not_in_allow",&types_not_in_allow_register},
-	{"spurius_audit_register",&spurious_audit_register},
-	{"attributes_not_in_rules",&attributes_not_in_rules_register},
+	{"find_domains",			&find_domains_register},
+	{"find_file_types", 		&find_file_types_register},
+	{"domain_and_file_type",	&domain_and_file_type_register},
+	{"attributes_wo_types",		&attributes_wo_types_register},
+	{"roles_wo_types",			&roles_wo_types_register},
+	{"users_wo_roles",			&users_wo_roles_register},
+	{"roles_not_in_allow",		&roles_not_in_allow_register},
+	{"types_not_in_allow",		&types_not_in_allow_register},
+	{"spurious_audit",			&spurious_audit_register},
+	{"attributes_not_in_rules",	&attributes_not_in_rules_register},
+
 	/* TODO: add additional register addresses here */
-	{NULL,NULL}
+
+	{NULL, NULL}
 };
 
 int sechk_register_list_get_num_modules()
@@ -35,4 +37,9 @@ int sechk_register_list_get_num_modules()
 
 	sechk_register_num_modules = i;
 	return sechk_register_num_modules;
+}
+
+const sechk_module_name_reg_t *sechk_register_list_get_known_modules()
+{
+	return sechk_register_list;
 }
