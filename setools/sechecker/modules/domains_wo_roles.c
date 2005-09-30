52df18a3aed95035f220ab2102a8f12880c10868
kcarr <kcarr@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2005-09-30 20:16:04 +0000

    adding modules to sechecker and new analysis profile

--- setools/sechecker/modules/domains_wo_roles.c
+++ setools/sechecker/modules/domains_wo_roles.c
@@ -1,15 +1,14 @@
 /* Copyright (C) 2005 Tresys Technology, LLC
  * see file 'COPYING' for use and warranty information */
- 
-/* 
+
+/*
  * Author: jmowery@tresys.com
  *
  */
 
-
 #include "sechecker.h"
 #include "policy.h"
-#include "attributes_wo_types.h"
+#include "domains_wo_roles.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -21,11 +20,11 @@ static sechk_lib_t *library;
 /* This string is the name of the module and should match the stem
  * of the file name; it should also match the prefix of all functions
  * defined in this module and the private data storage structure */
-static const char *const mod_name = "attributes_wo_types";
+static const char *const mod_name = "domains_wo_roles";
 
 /* The register function registers all of a module's functions
  * with the library. */
-int attributes_wo_types_register(sechk_lib_t *lib) 
+int domains_wo_roles_register(sechk_lib_t *lib)
 {
 	sechk_module_t *mod = NULL;
 	sechk_fn_t *fn_struct = NULL;
@@ -47,20 +46,19 @@ int attributes_wo_types_register(sechk_lib_t *lib)
 	}
 	
 	/* assign the descriptions */
-	mod->brief_description = "Finds attributes in the policy with no types.";
-	mod->detailed_description = "Finds attributes in the policy not associated with any types."
-"\nAttributes without types can cause type fields in rules to expand to empty"
-"\nsets, which are discarded by the compiler. While this does not cause any"
-"\nadditional access, it may be misleading when reading the source."
-		"\n  Requirements:"
-		"\n    policy_type=source"
-		"\n  Dependencies:"
-		"\n    none"
-		"\n  Options:"
-		"\n    none";
+	mod->brief_description = "Finds all domains in the policy not associated with a role";
+	mod->detailed_description = "Finds all domains in the policy not associated with a role"
+"\nThese domians cannot have a valid security context."
+"\nThe role object_r is not considered in this check."
+"\n  Requirements:"
+"\n    none"
+"\n  Dependencies:"
+"\n    module=find_domains"
+"\n  Options:"
+"\n    none";
 
-	/* assign requirements */
-	mod->requirements = sechk_name_value_prepend(NULL,"policy_type","source");
+	/* assign dependencies */
+	mod->dependencies = sechk_name_value_prepend(NULL,"module","find_domains");
 
 	/* register functions */
 	fn_struct = sechk_fn_new();
@@ -73,7 +71,7 @@ int attributes_wo_types_register(sechk_lib_t *lib)
 		fprintf(stderr, "Error: out of memory\n");
 		return -1;
 	}
-	fn_struct->fn = &attributes_wo_types_init;
+	fn_struct->fn = &domains_wo_roles_init;
 	fn_struct->next = mod->functions;
 	mod->functions = fn_struct;
 
@@ -87,7 +85,7 @@ int attributes_wo_types_register(sechk_lib_t *lib)
 		fprintf(stderr, "Error: out of memory\n");
 		return -1;
 	}
-	fn_struct->fn = &attributes_wo_types_run;
+	fn_struct->fn = &domains_wo_roles_run;
 	fn_struct->next = mod->functions;
 	mod->functions = fn_struct;
 
@@ -101,7 +99,7 @@ int attributes_wo_types_register(sechk_lib_t *lib)
 		fprintf(stderr, "Error: out of memory\n");
 		return -1;
 	}
-	fn_struct->fn = &attributes_wo_types_free;
+	fn_struct->fn = &domains_wo_roles_free;
 	fn_struct->next = mod->functions;
 	mod->functions = fn_struct;
 
@@ -115,7 +113,7 @@ int attributes_wo_types_register(sechk_lib_t *lib)
 		fprintf(stderr, "Error: out of memory\n");
 		return -1;
 	}
-	fn_struct->fn = &attributes_wo_types_print_output;
+	fn_struct->fn = &domains_wo_roles_print_output;
 	fn_struct->next = mod->functions;
 	mod->functions = fn_struct;
 
@@ -129,23 +127,13 @@ int attributes_wo_types_register(sechk_lib_t *lib)
 		fprintf(stderr, "Error: out of memory\n");
 		return -1;
 	}
-	fn_struct->fn = &attributes_wo_types_get_result;
+	fn_struct->fn = &domains_wo_roles_get_result;
 	fn_struct->next = mod->functions;
 	mod->functions = fn_struct;
 
-	fn_struct = sechk_fn_new();
-	if (!fn_struct) {
-		fprintf(stderr, "Error: out of memory\n");
-		return -1;
-	}
-	fn_struct->name = strdup("get_list");
-	if (!fn_struct->name) {
-		fprintf(stderr, "Error: out of memory\n");
-		return -1;
-	}
-	fn_struct->fn = &attributes_wo_types_get_list;
-	fn_struct->next = mod->functions;
-	mod->functions = fn_struct;
+	/* TODO: (optional) add any other functions needed here,
+	 * add a block as above for each additional function */
+
 
 	return 0;
 }
@@ -153,10 +141,10 @@ int attributes_wo_types_register(sechk_lib_t *lib)
 /* The init function creates the module's private data storage object
  * and initializes its values based on the options parsed in the config
  * file. */
-int attributes_wo_types_init(sechk_module_t *mod, policy_t *policy)
+int domains_wo_roles_init(sechk_module_t *mod, policy_t *policy)
 {
 	sechk_name_value_t *opt = NULL;
-	attributes_wo_types_data_t *datum = NULL;
+	domains_wo_roles_data_t *datum = NULL;
 
 	if (!mod || !policy) {
 		fprintf(stderr, "Error: invalid parameters\n");
@@ -167,7 +155,7 @@ int attributes_wo_types_init(sechk_module_t *mod, policy_t *policy)
 		return -1;
 	}
 
-	datum = attributes_wo_types_data_new();
+	datum = domains_wo_roles_data_new();
 	if (!datum) {
 		fprintf(stderr, "Error: out of memory\n");
 		return -1;
@@ -183,15 +171,20 @@ int attributes_wo_types_init(sechk_module_t *mod, policy_t *policy)
 }
 
 /* The run function performs the check. This function runs only once
- * even if called multiple times. This function allocates the result
- * structure and fills in all relavant item and proof data. */
-int attributes_wo_types_run(sechk_module_t *mod, policy_t *policy)
+ * even if called multiple times. */
+int domains_wo_roles_run(sechk_module_t *mod, policy_t *policy)
 {
-	attributes_wo_types_data_t *datum;
+	domains_wo_roles_data_t *datum;
 	sechk_result_t *res = NULL;
 	sechk_item_t *item = NULL;
 	sechk_proof_t *proof = NULL;
-	int i, retv, num_types = 0, *types = NULL;
+	int *domain_list = NULL, domain_list_sz = 0;
+	int i, j, retv;
+	sechk_module_t *mod_ptr = NULL;
+	sechk_run_fn_t run_fn = NULL;
+	int (*get_list_fn)(sechk_module_t *mod, int **array, int *size) = NULL;
+	bool_t used = FALSE;
+	int object_r_idx = -1;
 
 	if (!mod || !policy) {
 		fprintf(stderr, "Error: invalid parameters\n");
@@ -206,7 +199,7 @@ int attributes_wo_types_run(sechk_module_t *mod, policy_t *policy)
 	if (mod->result)
 		return 0;
 
-	datum = (attributes_wo_types_data_t*)mod->data;
+	datum = (domains_wo_roles_data_t*)mod->data;
 	res = sechk_result_new();
 	if (!res) {
 		fprintf(stderr, "Error: out of memory\n");
@@ -215,46 +208,63 @@ int attributes_wo_types_run(sechk_module_t *mod, policy_t *policy)
 	res->test_name = strdup(mod_name);
 	if (!res->test_name) {
 		fprintf(stderr, "Error: out of memory\n");
-		goto attributes_wo_types_run_fail;
+		goto domains_wo_roles_run_fail;
 	}
-	res->item_type = POL_LIST_ATTRIB;
+	res->item_type = POL_LIST_TYPE;
 
-	for (i = policy->num_attribs - 1; i >= 0; i--) {
-		num_types = 0;
-		free(types);
-		types = NULL;
-		retv = get_attrib_types(i, &num_types, &types, policy);
-		if (retv) {
-			fprintf(stderr, "Error: out of memory\n");
-			goto attributes_wo_types_run_fail;
+	run_fn = sechk_lib_get_module_function("find_domains", SECHK_MOD_FN_RUN, library);
+	if (!run_fn)
+		goto domains_wo_roles_run_fail;
+	get_list_fn = sechk_lib_get_module_function("find_domains", "get_list", library);
+	if (!get_list_fn)
+		goto domains_wo_roles_run_fail;
+
+	retv = run_fn((mod_ptr = sechk_lib_get_module("find_domains", library)), policy);
+	if (retv) {
+		fprintf(stderr, "Error: dependency failed\n");
+		goto domains_wo_roles_run_fail;
+	}
+	retv = get_list_fn(mod_ptr, &domain_list, &domain_list_sz);
+	if (retv) {
+		fprintf(stderr, "Error: unable to get list\n");
+		goto domains_wo_roles_run_fail;
+	}
+
+	object_r_idx = get_role_idx("object_r", policy);
+
+	for (i = 0; i < domain_list_sz; i++) {
+		used = FALSE;
+		for (j = 0; j < policy->num_roles; j++) {
+			if (j == object_r_idx)
+				continue;
+			if (does_role_use_type(j, domain_list[i], policy)) {
+				used = TRUE;
+				break;
+			}
 		}
-		if (num_types) 
+		if (used)
 			continue;
-		proof = sechk_proof_new();
-		if (!proof) {
-			fprintf(stderr, "Error: out of memory\n");
-			goto attributes_wo_types_run_fail;
-		}
-		proof->idx = i;
-		proof->type = POL_LIST_ATTRIB;
-		proof->severity = SECHK_SEV_LOW;
-		proof->text = (char*)calloc(strlen("attribute  has no types")+strlen(policy->attribs[i].name)+1, sizeof(char));
-		sprintf(proof->text, "attribute %s has no types", policy->attribs[i].name);
 		item = sechk_item_new();
 		if (!item) {
 			fprintf(stderr, "Error: out of memory\n");
-			goto attributes_wo_types_run_fail;
+			goto domains_wo_roles_run_fail;
 		}
-		item->item_id = i;
+		item->item_id = domain_list[i];
 		item->test_result = 1;
-		proof->next = item->proof;
-		item->proof = proof;
+		item->proof = sechk_proof_new();
+		item->proof->idx = -1;
+		item->proof->type = POL_LIST_ROLES;
+		item->proof->severity = SECHK_SEV_MOD;
+		item->proof->text = strdup("Type is a domain but not associated with a role.");
+		if (!item->proof->text) {
+			fprintf(stderr, "Error: out of memory\n");
+			goto domains_wo_roles_run_fail;
+		}
 		item->next = res->items;
 		res->items = item;
-		(res->num_items)++;
+		res->num_items++;
+		item = NULL;
 	}
-
-
 	mod->result = res;
 
 	if (res->num_items > 0)
@@ -262,8 +272,8 @@ int attributes_wo_types_run(sechk_module_t *mod, policy_t *policy)
 
 	return 0;
 
-attributes_wo_types_run_fail:
-	free(types);
+domains_wo_roles_run_fail:
+	/* TODO: free any other memory allocated during check logic */
 	sechk_proof_free(proof);
 	sechk_item_free(item);
 	sechk_result_free(res);
@@ -271,9 +281,9 @@ attributes_wo_types_run_fail:
 }
 
 /* The free function frees the private data of a module */
-void attributes_wo_types_free(sechk_module_t *mod) 
+void domains_wo_roles_free(sechk_module_t *mod)
 {
-	attributes_wo_types_data_t *datum;
+	domains_wo_roles_data_t *datum;
 
 	if (!mod) {
 		fprintf(stderr, "Error: invalid parameters\n");
@@ -284,24 +294,24 @@ void attributes_wo_types_free(sechk_module_t *mod)
 		return;
 	}
 
-	datum = (attributes_wo_types_data_t*)mod->data;
+	datum = (domains_wo_roles_data_t*)mod->data;
 
 	free(mod->data);
 	mod->data = NULL;
 }
 
 /* The print output function generates the text printed in the
- * report and prints it to stdout. */
-int attributes_wo_types_print_output(sechk_module_t *mod, policy_t *policy) 
+ * report and prints it to stdout.  */
+int domains_wo_roles_print_output(sechk_module_t *mod, policy_t *policy) 
 {
-	attributes_wo_types_data_t *datum = NULL;
+	domains_wo_roles_data_t *datum = NULL;
 	unsigned char outformat = 0x00;
 	sechk_item_t *item = NULL;
 	sechk_proof_t *proof = NULL;
 	int i = 0;
 
-        if (!mod || (!policy && (mod->outputformat & ~(SECHK_OUT_BRF_DESCP) &&
-                                 (mod->outputformat & ~(SECHK_OUT_DET_DESCP))))){
+	if (!mod || (!policy && (mod->outputformat & ~(SECHK_OUT_BRF_DESCP) &&
+				 (mod->outputformat & ~(SECHK_OUT_DET_DESCP))))) {
 		fprintf(stderr, "Error: invalid parameters\n");
 		return -1;
 	}
@@ -310,7 +320,7 @@ int attributes_wo_types_print_output(sechk_module_t *mod, policy_t *policy)
 		return -1;
 	}
 
-	datum = (attributes_wo_types_data_t*)mod->data;
+	datum = (domains_wo_roles_data_t*)mod->data;
 	outformat = mod->outputformat;
 
 	if (!mod->result && (outformat & ~(SECHK_OUT_BRF_DESCP)) && (outformat & ~(SECHK_OUT_DET_DESCP))) {
@@ -330,9 +340,8 @@ int attributes_wo_types_print_output(sechk_module_t *mod, policy_t *policy)
 	if (outformat & SECHK_OUT_DET_DESCP) {
 		printf("%s\n\n", mod->detailed_description);
 	}
-	/* display the statistics of the results */
 	if (outformat & SECHK_OUT_STATS) {
-		printf("Found %i attributes.\n", mod->result->num_items);
+		printf("Found %i types.\n", mod->result->num_items);
 	}
 	/* The list report component is a display of all items
 	 * found without any supporting proof. */
@@ -341,7 +350,7 @@ int attributes_wo_types_print_output(sechk_module_t *mod, policy_t *policy)
 		for (item = mod->result->items; item; item = item->next) {
 			i++;
 			i %= 4;
-			printf("%s%s", policy->attribs[item->item_id].name, (i ? ", " : "\n")); 
+			printf("%s%s", policy->types[item->item_id].name, (i ? ", " : "\n")); 
 		}
 		printf("\n");
 	}
@@ -349,13 +358,12 @@ int attributes_wo_types_print_output(sechk_module_t *mod, policy_t *policy)
 	 * with an indented list of proof statements supporting the result
 	 * of the check for that item (e.g. rules with a given type)
 	 * this field also lists the computed severity of each item
-	 * items are printed on a line either with (or, if long, such as a
-	 * rule, followed by) the severity. Each proof element is then
-	 * displayed in an indented list one per line below it. */
+	 * (see sechk_item_sev in sechecker.c for details on calculation)
+	 * items are printed on a line either with the severity. */
 	if (outformat & SECHK_OUT_PROOF) {
 		printf("\n");
 		for (item = mod->result->items; item; item = item->next) {
-			printf("%s", policy->attribs[item->item_id].name);
+			printf("%s", policy->types[item->item_id].name);
 			printf(" - severity: %s\n", sechk_item_sev(item));
 			for (proof = item->proof; proof; proof = proof->next) {
 				printf("\t%s\n", proof->text);
@@ -369,7 +377,7 @@ int attributes_wo_types_print_output(sechk_module_t *mod, policy_t *policy)
 
 /* The get_result function returns a pointer to the results
  * structure for this check to be used in another check. */
-sechk_result_t *attributes_wo_types_get_result(sechk_module_t *mod) 
+sechk_result_t *domains_wo_roles_get_result(sechk_module_t *mod) 
 {
 
 	if (!mod) {
@@ -384,47 +392,16 @@ sechk_result_t *attributes_wo_types_get_result(sechk_module_t *mod)
 	return mod->result;
 }
 
-/* The attributes_wo_types_data_new function allocates and returns an
+/* The domains_wo_roles_data_new function allocates and returns an
  * initialized private data storage structure for this
  * module. */
-attributes_wo_types_data_t *attributes_wo_types_data_new(void)
+domains_wo_roles_data_t *domains_wo_roles_data_new(void)
 {
-	attributes_wo_types_data_t *datum = NULL;
+	domains_wo_roles_data_t *datum = NULL;
 
-	datum = (attributes_wo_types_data_t*)calloc(1,sizeof(attributes_wo_types_data_t));
+	datum = (domains_wo_roles_data_t*)calloc(1,sizeof(domains_wo_roles_data_t));
 
 	return datum;
 }
 
-int attributes_wo_types_get_list(sechk_module_t *mod, int **array, int *size)
-{
-	int i;
-	sechk_item_t *item = NULL;
-
-	if (!mod || !array || !size) {
-		fprintf(stderr, "Error: invalid parameters\n");
-		return -1;
-	}
-	if (strcmp(mod_name, mod->name)) {
-		fprintf(stderr, "Error: wrong module (%s)\n", mod->name);
-		return -1;
-	}
-	if (!mod->result) {
-		fprintf(stderr, "Error: module has not been run\n");
-		return -1;
-	}
-
-	*size = mod->result->num_items;
-
-	*array = (int*)malloc(mod->result->num_items * sizeof(int));
-	if (!(*array)) {
-		fprintf(stderr, "Error: out of memory\n");
-		return -1;
-	}
-
-	for (i = 0, item = mod->result->items; item && i < *size; i++, item = item->next) {
-		(*array)[i] = item->item_id;
-	}
-
-	return 0;
-}
+ 
