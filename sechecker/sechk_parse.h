9dd4f2cbf0718ac30fdf5b841f102bb8fded1382
kcarr <kcarr@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2005-10-19 21:22:12 +0000

    sechecker: modified profile format

--- sechecker/sechk_parse.h
+++ sechecker/sechk_parse.h
@@ -14,19 +14,8 @@
 #include <libxml/xmlreader.h>
 #include <assert.h>
 
-/* xml parser keywords */
-#define SECHK_PARSE_SECHECKER_TAG         "sechecker"
-#define SECHK_PARSE_PROFILE_TAG           "profile"
-#define SECHK_PARSE_MODULE_TAG            "module"
-#define SECHK_PARSE_OPTION_TAG            "option"
-#define SECHK_PARSE_OUTPUT_TAG            "output"
-#define SECHK_PARSE_VALUE_ATTRIB          "value"
-#define SECHK_PARSE_NAME_ATTRIB           "name"
-#define SECHK_PARSE_VERSION_ATTRIB        "version"
-#define SECHK_PARSE_OUTPUT_NONE           "none"
-#define SECHK_PARSE_OUTPUT_QUIET          "quiet"
-#define SECHK_PARSE_OUTPUT_SHORT        "short"
-#define SECHK_PARSE_OUTPUT_VERBOSE        "verbose"
+/* sechecker parser keywords - this subset is exported because
+ * they are predefined 'values' also used in the lib */
 #define SECHK_PARSE_REQUIRE_POL_TYPE      "policy_type"
 #define SECHK_PARSE_REQUIRE_POL_TYPE_SRC  "source"
 #define SECHK_PARSE_REQUIRE_POL_TYPE_BIN  "binary"
@@ -35,7 +24,6 @@
 #define SECHK_PARSE_REQUIRE_MLS_POLICY    "mls_policy"
 #define SECHK_PARSE_REQUIRE_MLS_SYSTEM    "mls_system"
 
-#define sechk_lib_parse_config_file(path, lib) sechk_lib_parse_xml_file(path, lib)
 #define sechk_lib_parse_profile(path, lib)     sechk_lib_parse_xml_file(path, lib)
 
 int sechk_lib_parse_xml_file(const char *filename, sechk_lib_t *lib);
