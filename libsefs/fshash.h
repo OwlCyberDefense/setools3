2cb77ad13a44fd72e735bb40ff6112b0e17e0a41
kcarr <kcarr@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2005-11-15 16:48:59 +0000

    replcon: do not relable bind mounts case 315

---
+++ libsefs/fshash.h
@@ -0,0 +1,27 @@
+/* Copyright (C) 2005 Tresys Technology, LLC
+ * see file 'COPYING' for use and warranty information */
+
+/* fshash.h
+ *
+ * hash fcns for bind mount hashing 
+ *
+ */
+#ifndef _FS_HASH_H
+#define _FS_HASH_H
+
+typedef struct sefs_hash_node {
+	struct sefs_hash_node *next;
+	char *key;               
+} sefs_hash_node_t;
+
+typedef struct sefs_hash {
+	sefs_hash_node_t **table;
+	int size;
+} sefs_hash_t;
+
+sefs_hash_t *sefs_hash_new(int size);
+int sefs_hash_insert(sefs_hash_t *hashtab, const char *key);
+int sefs_hash_find(sefs_hash_t *hashtab, const char *key);
+void sefs_hash_destroy(sefs_hash_t *hashtab);
+
+#endif /* _FS_HASH_H */
