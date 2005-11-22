366e10a8919d7d21baffa5a979812b2c46213fd5
Kevin Carr <kcarr@tresys.com>
2005-11-22 18:14:40 +0000

    apol: added new range_trans rules tab case 1259

---
+++ setools/libapol/apol_tcl_render.h
@@ -0,0 +1,21 @@
+ /* Copyright (C) 2003-2005 Tresys Technology, LLC
+ * see file 'COPYING' for use and warranty information */
+
+/* 
+ * Author: Jason Tang <jtang@tresys.com>, Kevin Carr <kcarr@tresys.com>
+ */
+
+/* tcl_render.h */
+
+/* This file takes various policy stuff and returns formatted Tcl
+   lists, suitable for displaying results in Apol. */
+
+#include "policy.h"
+#include <tcl.h>
+
+#ifndef AP_TCL_RENDER_H
+#define AP_TCL_RENDER_H
+
+int tcl_render_rangetrans(Tcl_Interp *interp, bool_t addlineno, int idx, policy_t *policy);
+
+#endif
