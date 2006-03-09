afe7ea62b0e124775b02a46ca8bf97d9e05b4cea
Kevin Carr <kcarr@tresys.com>
2006-03-09 19:10:22 +0000

    sechecker: adding net_access module Bugz: 1380

--- setools/sechecker/modules/find_net_domains.h
+++ setools/sechecker/modules/find_net_domains.h
@@ -25,6 +25,7 @@ int find_net_domains_run(sechk_module_t *mod, policy_t *policy);
 void find_net_domains_free(sechk_module_t *mod);
 int find_net_domains_print_output(sechk_module_t *mod, policy_t *policy);
 sechk_result_t *find_net_domains_get_result(sechk_module_t *mod);
+int find_net_domains_get_list(sechk_module_t *mod, int **array, int *size);
 
 /* The following function is used to allocate and initialize
  * the private data storage structure for this module */
