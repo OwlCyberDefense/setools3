d0b944016d51f11ab06ab05a9060944278a2bb32
kcarr <kcarr@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2006-04-11 20:27:18 +0000

    mls support: fix indexcon, searchcon, and apol fc tab to search MLS filesystem databases

--- libapol/apol_tcl_fc.c
+++ libapol/apol_tcl_fc.c
@@ -126,235 +126,230 @@ static int Apol_Close_FC_Index_DB(ClientData clientData, Tcl_Interp *interp, int
 }
 
 #ifdef LIBSEFS
-static void apol_search_fc_index_append_results(sefs_search_ret_t *key, Tcl_Interp *interp) 
+static int append_search_fc_index_to_list(Tcl_Interp *interp, sefs_search_ret_t *key, Tcl_Obj *result_list)
 {
 	sefs_search_ret_t *curr = key;
 	
 	/* walk the linked list */
 	while (curr) {
-		if (curr->path)
-			Tcl_AppendElement(interp, curr->path);
-		if (curr->context)
-			Tcl_AppendElement(interp, curr->context);
-		if (curr->object_class)
-			Tcl_AppendElement(interp, curr->object_class);
+		Tcl_Obj *fscon[3], *fscon_list;
+		if (curr->context) {
+			fscon[0] = Tcl_NewStringObj(curr->context, -1);
+		}
+		else {
+			fscon[0] = Tcl_NewStringObj("", -1);
+		}
+		if (curr->object_class) {
+			fscon[1] = Tcl_NewStringObj(curr->object_class, -1);
+		}
+		else {
+			fscon[1] = Tcl_NewStringObj("", -1);
+		}
+		if (curr->path) {
+			fscon[2] = Tcl_NewStringObj(curr->path, -1);
+		}
+		else {
+			fscon[2] = Tcl_NewStringObj("", -1);
+		}
+		fscon_list = Tcl_NewListObj(3, fscon);
+		if (Tcl_ListObjAppendElement(interp, result_list, fscon_list) == TCL_ERROR) {
+			return TCL_ERROR;
+		}
 		curr = curr->next;
 	}
+	return TCL_OK;
 }
 #endif
 
 
-/*
- * This function expects a file context index to be loaded into memory.
- * Arguments:
- *	argv[1] - use_type [bool]
- * 	argv[2] - type [TCL list of strings]
- * 	argv[3] - use_user [bool]
- *	argv[4] - user [TCL list of strings]
- * 	argv[5] - use_class [bool]
- * 	argv[6] - object class [TCL list of strings]
- * 	argv[7] - use_path [bool]
- * 	argv[8] - path [Tcl list of strings]
- * 	argv[9] - use regular expressions for user
- * 	argv[10] - use regular expressions for type
- *	argv[11] - use regular expressions for path
+/**
+ * Assuming that the file contexts database has already been open,
+ * return a list of file contexts (3-ple of context, object class,
+ * path) that match the given criteria.
+ *	argv[1] - list of user strings
+ *	argv[2] - list of type strings
+ *	argv[3] - list of object class strings
+ *	argv[4] - list of MLS ranges
+ *	argv[5] - list of path strings
+ *	argv[6] - use regular expressions for user
+ *	argv[7] - use regular expressions for type
+ *	argv[8] - use regular expressions for MLS ranges
+ *	argv[9] - use regular expressions for path
  */
 static int Apol_Search_FC_Index_DB(ClientData clientData, Tcl_Interp *interp, int argc, CONST char *argv[])
 {	
-	if(argc != 12) {
-		Tcl_AppendResult(interp, "wrong # of args", (char *) NULL);
-		return TCL_ERROR;
-	}
+	sefs_search_keys_t search_keys;
+	CONST char **object_classes = NULL, **types = NULL, **users = NULL,
+	    **ranges, **paths = NULL;
+	int retval = TCL_ERROR;
+	Tcl_Obj *result_obj = Tcl_NewListObj(0, NULL);
+
 #ifndef LIBSEFS
-	Tcl_AppendResult(interp, "You need to build apol with libsefs to use this feature!", (char *) NULL);
+	Tcl_SetResult(interp, "You need to build apol with libsefs to use this feature!", TCL_STATIC);
 	return TCL_ERROR;
 #else		
-	int rt;
-	int num_types, num_users, num_classes, num_paths;
-	sefs_search_keys_t search_keys;
-	CONST char **object_classes, **types, **users, **paths;
+	memset(&search_keys, 0, sizeof(search_keys));
+	if (argc != 10) {
+		Tcl_SetResult(interp, "Need a list of users, list of types, list of object classes, list of ranges, list of paths, user_regex, type_regex, range_regex, and path_regex", TCL_STATIC);
+		goto cleanup;
+	}
 	
 	if (fsdata == NULL) {
-		Tcl_AppendResult(interp, "No Index File Loaded!", (char *) NULL);
-		return TCL_ERROR;
-	}
-	object_classes = types = users = paths = NULL;
-
-	search_keys.user = NULL;
-	search_keys.path = NULL;
-	search_keys.type = NULL;
-	search_keys.object_class = NULL;
-	search_keys.num_type = 0;
-	search_keys.num_user = 0;
-	search_keys.num_object_class = 0;
-	search_keys.num_path = 0;
-	
-	if (getbool(argv[1])) {
-		rt = Tcl_SplitList(interp, argv[2], &num_types, &types);
-		if (rt != TCL_OK) {
-			Tcl_AppendResult(interp, "Error splitting TCL list.", (char *) NULL);
-			goto err;
-		}
-		
-		if (num_types < 1) {
-			Tcl_AppendResult(interp, "Must provide at least 1 type.", (char *) NULL);
-			goto err;
-		}
-		search_keys.num_type = num_types;
-		search_keys.type = types;
-	}
-	if (getbool(argv[3])) {
-		rt = Tcl_SplitList(interp, argv[4], &num_users, &users);
-		if (rt != TCL_OK) {
-			Tcl_AppendResult(interp, "Error splitting TCL list.", (char *) NULL);
-			goto err;
-		}
-		
-		if (num_users < 1) {
-			Tcl_AppendResult(interp, "Must provide at least 1 user.", (char *) NULL);
-			goto err;
-		}
-		search_keys.num_user = num_users;
-		search_keys.user = users;
-	}
-	if (getbool(argv[5])) {
-		rt = Tcl_SplitList(interp, argv[6], &num_classes, &object_classes);
-		if (rt != TCL_OK) {
-			Tcl_AppendResult(interp, "Error splitting TCL list.", (char *) NULL);
-			goto err;
-		}
-		
-		if (num_classes < 1) {
-			Tcl_AppendResult(interp, "Must provide at least 1 object class.", (char *) NULL);
-			goto err;
-		}
-		search_keys.num_object_class = num_classes;
-		search_keys.object_class = object_classes;
-	}
-	if (getbool(argv[7])) {
-		rt = Tcl_SplitList(interp, argv[8], &num_paths, &paths);
-		if (rt != TCL_OK) {
-			Tcl_AppendResult(interp, "Error splitting TCL list.", (char *) NULL);
-			goto err;
-		}
-		
-		if (num_paths < 1) {
-			Tcl_AppendResult(interp, "Must provide at least 1 path.", (char *) NULL);
-			goto err;
-		}
-		search_keys.num_path = num_paths;
-		search_keys.path = paths;
+		Tcl_SetResult(interp, "No Index File Loaded!", TCL_STATIC);
+		goto cleanup;
 	}
 
-	rt = Tcl_GetInt(interp, argv[9], &search_keys.do_user_regEx);
-	if (rt == TCL_ERROR) {
-		Tcl_AppendResult(interp, "argv[9] apparently is not an integer", (char *) NULL);
-		return TCL_ERROR;
+	if (Tcl_SplitList(interp, argv[1], &search_keys.num_user, &users) == TCL_ERROR ||
+	    Tcl_SplitList(interp, argv[2], &search_keys.num_type, &types) == TCL_ERROR ||
+	    Tcl_SplitList(interp, argv[3], &search_keys.num_object_class, &object_classes) == TCL_ERROR ||
+	    Tcl_SplitList(interp, argv[4], &search_keys.num_range, &ranges) == TCL_ERROR ||
+	    Tcl_SplitList(interp, argv[5], &search_keys.num_path, &paths) == TCL_ERROR) {
+		goto cleanup;
 	}
-	rt = Tcl_GetInt(interp, argv[10], &search_keys.do_type_regEx);
-	if (rt == TCL_ERROR) {
-		Tcl_AppendResult(interp, "argv[10] apparently is not an integer", (char *) NULL);
-		return TCL_ERROR;
+	search_keys.user = users;
+	search_keys.type = types;
+	search_keys.object_class = object_classes;
+	search_keys.range = ranges;
+	search_keys.path = paths;
+	if (Tcl_GetInt(interp, argv[6], &search_keys.do_user_regEx) == TCL_ERROR ||
+	    Tcl_GetInt(interp, argv[7], &search_keys.do_type_regEx) == TCL_ERROR ||
+	    Tcl_GetInt(interp, argv[8], &search_keys.do_range_regEx) == TCL_ERROR ||
+	    Tcl_GetInt(interp, argv[9], &search_keys.do_path_regEx) == TCL_ERROR) {
+		goto cleanup;
 	}
-	rt = Tcl_GetInt(interp, argv[11], &search_keys.do_path_regEx);
-	if (rt == TCL_ERROR) {
-		Tcl_AppendResult(interp, "argv[11] apparently is not an integer", (char *) NULL);
-		return TCL_ERROR;
+
+	if (sefs_filesystem_db_search(fsdata, &search_keys) != 0) {
+		Tcl_SetResult(interp, "FC search failed.", TCL_STATIC);
+		goto cleanup;
 	}
-	
-	if (!search_keys.type && !search_keys.user && !search_keys.object_class && !search_keys.path) {
-		Tcl_AppendResult(interp, "You must specify search criteria!", (char *) NULL);
-		goto err;
+	if (append_search_fc_index_to_list(interp, search_keys.search_ret, result_obj) == TCL_ERROR) {
+		goto cleanup;
 	}
-	
-	rt = sefs_filesystem_db_search(fsdata, &search_keys);
-	if (rt != 0) {
-		Tcl_AppendResult(interp, "Search failed\n", (char *) NULL);
-		goto err;
-	}
-	apol_search_fc_index_append_results(search_keys.search_ret, interp);
+
+	Tcl_SetObjResult(interp, result_obj);
+	retval = TCL_OK;
+ cleanup:
 	sefs_search_keys_ret_destroy(search_keys.search_ret);
-	if (types) Tcl_Free((char *) types);
 	if (users) Tcl_Free((char *) users);
-	if (object_classes) Tcl_Free((char *) object_classes);
-	if (paths) Tcl_Free((char *) paths);
-	return TCL_OK;
-err:
 	if (types) Tcl_Free((char *) types);
-	if (users) Tcl_Free((char *) users);
 	if (object_classes) Tcl_Free((char *) object_classes);
+	if (ranges) Tcl_Free((char *) ranges);
 	if (paths) Tcl_Free((char *) paths);
-	return TCL_ERROR;
+	return retval;
 #endif
 }
 
-/* 
- * No arguments, however, this function expects a file context index to be loaded into memory.
+/**
+ * Assuming that the file contexts database has already been open,
+ * return a list of item names for a particular table.	Valid table
+ * names are: "types", "users", "classes", and "ranges".
  */
 static int Apol_FC_Index_DB_Get_Items(ClientData clientData, Tcl_Interp *interp, int argc, CONST char *argv[])
 {
-	if(argc != 2) {
-		Tcl_AppendResult(interp, "wrong # of args", (char *) NULL);
+	if (argc != 2) {
+		Tcl_SetResult(interp, "Need one of \"types\", \"users\", \"classes\", or \"ranges\".", TCL_STATIC);
 		return TCL_ERROR;
 	}
 #ifndef LIBSEFS
-	Tcl_AppendResult(interp, "You need to build apol with libsefs!", (char *) NULL);
+	Tcl_SetResult(interp, "You need to build apol with libsefs!", TCL_STATIC);
 	return TCL_ERROR;
 #else		
 	int list_sz = 0, i, request_type;
 	char **list_ret = NULL;
+	Tcl_Obj *result_obj = Tcl_NewListObj(0, NULL);
 	
 	if (fsdata == NULL) {
-		Tcl_AppendResult(interp, "No Index File Loaded!", (char *) NULL);
+		Tcl_SetResult(interp, "No Index File Loaded!", TCL_STATIC);
 		return TCL_ERROR;
-	}
-	
-	if(strcmp("types", argv[1]) == 0) {
-		request_type = SEFS_TYPES;
-	} else if(strcmp("users", argv[1]) == 0) {
-		request_type = SEFS_USERS;
-	} else if(strcmp("classes", argv[1]) == 0) {
-		request_type = SEFS_OBJECTCLASS;
-	}  else {
-		Tcl_AppendResult(interp, "Invalid option: ", argv[1], (char *) NULL);
-		return TCL_ERROR;
-	}
-	
- 	if ((list_ret = sefs_filesystem_db_get_known(fsdata, &list_sz, request_type)) != NULL) {
-		for (i = 0; i < list_sz; i++){
-			Tcl_AppendElement(interp, list_ret[i]);
-		}
-		sefs_double_array_destroy(list_ret, list_sz);
 	}
 
+	if (strcmp("types", argv[1]) == 0) {
+		request_type = SEFS_TYPES;
+	}
+	else if (strcmp("users", argv[1]) == 0) {
+		request_type = SEFS_USERS;
+	}
+	else if (strcmp("classes", argv[1]) == 0) {
+		request_type = SEFS_OBJECTCLASS;
+	}
+	else if (strcmp("ranges", argv[1]) == 0) {
+		int retval = sefs_filesystem_db_is_mls(fsdata);
+		if (retval < 0) {
+			Tcl_SetResult(interp, "Error determining if database is MLS.", TCL_STATIC);
+			return TCL_ERROR;
+		}
+		else if (retval == 0) {
+			/* database does not have MLS, so nothing to return */
+			Tcl_SetObjResult(interp, result_obj);
+			return TCL_OK;
+		}
+		request_type = SEFS_RANGES;
+	} else {
+		Tcl_SetResult(interp, "Need an item type.", TCL_STATIC);
+		return TCL_ERROR;
+	}
+
+	if ((list_ret = sefs_filesystem_db_get_known(fsdata, &list_sz, request_type)) == NULL) {
+		Tcl_SetResult(interp, "Error in getting items.", TCL_STATIC);
+		return TCL_ERROR;
+	}
+	for (i = 0; i < list_sz; i++){
+		Tcl_Obj *s = Tcl_NewStringObj(list_ret[i], -1);
+		if (Tcl_ListObjAppendElement(interp, result_obj, s) == TCL_ERROR) {
+			sefs_double_array_destroy(list_ret, list_sz);
+			return TCL_ERROR;
+		}
+	}
+	sefs_double_array_destroy(list_ret, list_sz);
+	Tcl_SetObjResult(interp, result_obj);
+	return TCL_OK;
+#endif
+}
+
+static int Apol_FC_Is_MLS(ClientData clientData, Tcl_Interp *interp, int argc, CONST char *argv[])
+{
+	int retval;
+	Tcl_Obj *result_obj;
+#ifndef LIBSEFS
+	Tcl_SetResult(interp, "You need to build apol with libsefs!", TCL_STATIC);
+	return TCL_ERROR;
+#else
+	if (fsdata == NULL) {
+		Tcl_SetResult(interp, "No Index File Loaded!", TCL_STATIC);
+		return TCL_ERROR;
+	}
+	retval = sefs_filesystem_db_is_mls(fsdata);
+	if (retval < 0) {
+		Tcl_SetResult(interp, "Error determining if database is MLS.", TCL_STATIC);
+		return TCL_ERROR;
+	}
+	result_obj = Tcl_NewIntObj(retval);
+	Tcl_SetObjResult(interp, result_obj);
 	return TCL_OK;
 #endif
 }
 
 static int Apol_IsLibsefs_BuiltIn(ClientData clientData, Tcl_Interp *interp, int argc, CONST char *argv[])
 {
-	char tbuf[64];
-	
-	if(argc != 1) {
-		Tcl_AppendResult(interp, "wrong # of args", (char *) NULL);
-		return TCL_ERROR;
+	Tcl_Obj *result_obj;
+	if (is_libsefs_builtin) {
+		result_obj = Tcl_NewIntObj(1);
 	}
-	if (is_libsefs_builtin) 
-		sprintf(tbuf, "%d", 1);
-	else 
-		sprintf(tbuf, "%d", 0);
-	Tcl_AppendElement(interp, tbuf);
+	else {
+		result_obj = Tcl_NewIntObj(0);
+	}
+	Tcl_SetObjResult(interp, result_obj);
 	return TCL_OK;
 }
-
 
 int ap_tcl_fc_init(Tcl_Interp *interp) {
 	Tcl_CreateCommand(interp, "apol_Create_FC_Index_File", Apol_Create_FC_Index_File, NULL, NULL);
 	Tcl_CreateCommand(interp, "apol_Load_FC_Index_File", Apol_Load_FC_Index_File, NULL, NULL);
-        Tcl_CreateCommand(interp, "apol_Close_FC_Index_DB", Apol_Close_FC_Index_DB, NULL, NULL);
+	Tcl_CreateCommand(interp, "apol_Close_FC_Index_DB", Apol_Close_FC_Index_DB, NULL, NULL);
 	Tcl_CreateCommand(interp, "apol_Search_FC_Index_DB", Apol_Search_FC_Index_DB, NULL, NULL);
 	Tcl_CreateCommand(interp, "apol_FC_Index_DB_Get_Items", Apol_FC_Index_DB_Get_Items, NULL, NULL);
+	Tcl_CreateCommand(interp, "apol_FC_Is_MLS", Apol_FC_Is_MLS, NULL, NULL);
 	Tcl_CreateCommand(interp, "apol_IsLibsefs_BuiltIn", Apol_IsLibsefs_BuiltIn, NULL, NULL);
 
-        return TCL_OK;
+	return TCL_OK;
 }
