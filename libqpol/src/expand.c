2f50d64f54854cbfd6626cefb7fe9d9e0b7b30ad
Kevin Carr <kcarr@tresys.com>
2006-06-07 20:30:58 +0000

    libqpol uses refactored expand in libsepol

--- libqpol/src/expand.c
+++ libqpol/src/expand.c
@@ -59,10 +59,9 @@ static int type_attr_map(hashtab_key_t key __attribute__ ((unused)), hashtab_dat
 int qpol_expand_module(qpol_handle_t *handle, qpol_policy_t *base)
 {
 	unsigned int i;
-	avrule_block_t *curblock;
 	uint32_t *typemap;
-	int rt;
 	policydb_t *db;
+	int rt;
 
 	if (handle == NULL || base == NULL) {
 		ERR(handle, "%s", strerror(EINVAL));
@@ -74,7 +73,6 @@ int qpol_expand_module(qpol_handle_t *handle, qpol_policy_t *base)
 	/* activate the global branch before expansion */
 	db->global->branch_list->enabled = 1;
 	db->global->enabled = db->global->branch_list;
-
 
 	/* expand out the types to include all the attributes */
 	if (hashtab_map(db->p_types.table, type_attr_map, (db))) {
@@ -92,29 +90,8 @@ int qpol_expand_module(qpol_handle_t *handle, qpol_policy_t *base)
 		typemap[i] = i+1;
 	}
 	
-	for (curblock = db->global; curblock != NULL; curblock = curblock->next) {
-		avrule_decl_t *decl = curblock->branch_list;
-		avrule_t *cur_avrule;
-
-		/* find the first decl thats enabled */
-		while (decl != NULL) {
-			if (decl->enabled)
-				break;
-			decl = decl->next;
-		}
-		if (decl == NULL) {
-			/* nothing was enabled within this block */
-			continue;
-		}
-		for (cur_avrule = decl->avrules; cur_avrule != NULL; cur_avrule = cur_avrule->next) {
-			/* FIXME: never allows are dropped by convert_and_expand */
-			if (convert_and_expand_rule(handle, db, db, typemap,
-						    cur_avrule, &db->te_avtab,
-						    NULL, NULL, 0) != 1) {
-				ERR(handle, "%s", strerror(errno));
-				goto err;
-			}
-		}
+	if (expand_module_helper(handle, db, db, typemap, 0) < 0) {
+		goto err;
 	}
 exit:
 	rt = 0;
