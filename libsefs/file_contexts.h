857ad45df0074dc635fbfecc7fa4deedb9675369
Randy Wicks <rwicks@tresys.com>
2006-06-26 14:25:33 +0000

    Updates libsefs's file_contexts functions to work properly with libapol/libqpol

--- libsefs/file_contexts.h
+++ libsefs/file_contexts.h
@@ -9,17 +9,45 @@
 #ifndef FILE_CONTEXTS_H
 #define FILE_CONTEXTS_H
 
-#include "policy.h"
+/* libapol */
+#include <../libapol/policy.h>
+#include <../libapol/policy-io.h>
+#include <render.h>
+#include "../libapol/vector.h"
+#include "policy-query.h"
+
+/* libqpol */
+#include <../libqpol/include/qpol/policy_query.h>
+
 #include <stdio.h>
 
+/* file type IDs */
+#define FILETYPE_NONE 0  /* none */
+/* the following values must correspond to libsepol flask.h */
+#define FILETYPE_REG  6  /* Regular file */
+#define FILETYPE_DIR  7  /* Directory */
+#define FILETYPE_LNK  9  /* Symbolic link */
+#define FILETYPE_CHR  10 /* Character device */
+#define FILETYPE_BLK  11 /* Block device */
+#define FILETYPE_SOCK 12 /* Socket */
+#define FILETYPE_FIFO 13 /* FIFO */
+#define FILETYPE_ANY  14 /* any type */
+
 /* general file contexts structure */
+typedef struct security_context {
+        qpol_user_t     	*user;
+        qpol_role_t     	*role;
+        qpol_type_t     	*type;
+        qpol_mls_range_t	*range;
+} security_con_t;
+
 typedef struct sefs_fc_entry {
 	char		*path;		/* the path for genfs_context, regexp for file_context */
 	int		filetype;	/* the type of file, block, char etc */
-//	security_con_t 	*context;  //FIXME
+	security_con_t 	*context; 
 } sefs_fc_entry_t; 
 
-int parse_file_contexts_file(const char *fc_path, sefs_fc_entry_t **contexts, int *num_contexts, policy_t *policy);
+int parse_file_contexts_file(const char *fc_path, apol_vector_t **contexts, int *num_contexts, apol_policy_t *policy);
 void sefs_fc_entry_free(sefs_fc_entry_t *fc_entry);
 int find_default_file_contexts_file(char **path);
 
