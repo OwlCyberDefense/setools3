bf9b8e811b06c88f7894d8144e1c456def0c3db6
Jason Mowery <jmowery@tresys.com>
2006-08-14 21:51:53 +0000

    LibPolDiff:    Initial skeleton check-in. Bugz: 1779

---
+++ libpoldiff/src/poldiff_internal.h
@@ -0,0 +1,40 @@
+/**
+ *  @file poldiff_internal.h
+ *  Protected Interface for computing a semantic policy difference.
+ *
+ *  @author Kevin Carr kcarr@tresys.com
+ *  @author Jeremy A. Mowery jmowery@tresys.com
+ *  @author Jason Tang jtang@tresys.com
+ *
+ *  Copyright (C) 2006 Tresys Technology, LLC
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#ifndef POLDIFF_POLDIFF_INTERNAL_H
+#define POLDIFF_POLDIFF_INTERNAL_H
+
+#include <apol/policy.h>
+#include <poldiff/poldiff.h>
+
+struct poldiff_state {
+	apol_policy_t *policy1;
+	apol_policy_t *policy2;
+	podiff_handle_fn_t
+	void *handle_arg;
+	uint32_t diff_status;
+}
+
+#endif /* POLDIFF_POLDIFF_INTERNAL_H */
