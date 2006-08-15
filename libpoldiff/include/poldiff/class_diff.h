48448d7cb79d1e3bc9850c68e0fae0a13a5b742e
Jason Mowery <jmowery@tresys.com>
2006-08-15 19:35:42 +0000

    LibPolDiff:    changes as per meeting on 20060815

--- libpoldiff/include/poldiff/class_diff.h
+++ libpoldiff/include/poldiff/class_diff.h
@@ -29,8 +29,8 @@
 #include <apol/vector.h>
 #include <poldiff/poldiff.h>
 
-typedef struct poldiff_class_diff_summary poldiff_class_diff_summary_t;
-typedef struct poldiff_class_diff poldiff_class_diff_t;
+typedef struct poldiff_class_summary poldiff_class_summary_t;
+typedef struct poldiff_class poldiff_class_t;
 
 /**
  *  Get the name of the class from a class diff.
@@ -39,16 +39,16 @@ typedef struct poldiff_class_diff poldiff_class_diff_t;
  *  @return name of the class on success and NULL on failure; if the call fails,
  *  errno will be set. The caller should not free the returned string.
  */
-extern char *poldiff_class_diff_get_name(poldiff_t *diff, poldiff_class_diff_t *cls);
+extern const char *poldiff_class_get_name(poldiff_t *diff, poldiff_class_t *cls);
 
 /**
- *  Get the type of difference from a class diff.
+ *  Get the form of difference from a class diff.
  *  @param diff The policy difference structure associated with the class diff.
- *  @param cls The class from which to get the difference type.
- *  @return the type of difference or DIFF_TYPE_NONE on error.
- *  If the call fails, errno will be set.
+ *  @param cls The class from which to get the difference form.
+ *  @return the form of difference (one of POLDIFF_FORM_*)or POLDIFF_FORM_NONE
+ *  on error. If the call fails, errno will be set.
  */
-extern poldiff_diff_type_e poldiff_class_diff_get_diff_type(poldiff_t *diff, poldiff_class_diff_t *cls);
+extern poldiff_form_e poldiff_class_get_form(poldiff_t *diff, poldiff_class_t *cls);
 
 /**
  *  Get a vector of the permissions added to the class.
@@ -58,7 +58,7 @@ extern poldiff_diff_type_e poldiff_class_diff_get_diff_type(poldiff_t *diff, pol
  *  in only policy 2 or NULL on error. If no permissions were added the size
  *  of the returned vector will be 0. On error, errno will be set.
  */
-extern apol_vector_t *poldiff_class_diff_get_added_perms(poldiff_t *diff, poldiff_class_diff_t *cls);
+extern apol_vector_t *poldiff_class_get_added_perms(poldiff_t *diff, poldiff_class_t *cls);
 
 /**
  *  Get a vector of the permissions removed from the class.
@@ -68,46 +68,40 @@ extern apol_vector_t *poldiff_class_diff_get_added_perms(poldiff_t *diff, poldif
  *  in only policy 1 or NULL on error. If no permissions were removed the size
  *  of the returned vector will be 0. On error, errno will be set.
  */
-extern apol_vector_t *poldiff_class_diff_get_removed_perms(poldiff_t *diff, poldiff_class_diff_t *cls);
+extern apol_vector_t *poldiff_class_get_removed_perms(poldiff_t *diff, poldiff_class_t *cls);
 
 /**
- *  Get the number of added classes from a class difference summary;
- *  @param diff The policy difference structure associated with the
- *  class difference summary.
- *  @param cds The class difference summary from which to get the number
- *  of added classes.
+ *  Get the number of added classes from a policy difference structure.
+ *  @param diff The policy difference structure from which to get the
+ *  number of added classes.
  *  @return the number of added classes or 0 if not yet run.
  */
-extern size_t poldiff_class_diff_summary_get_num_added_classes(poldiff_t *diff, poldiff_class_diff_summary_t *cds);
+extern size_t poldiff_get_num_added_classes(poldiff_t *diff);
 
 /**
- *  Get the number of removed classes from a class difference summary;
- *  @param diff The policy difference structure associated with the
- *  class difference summary.
- *  @param cds The class difference summary from which to get the number
- *  of removed classes.
+ *  Get the number of removed classes from a policy difference structure.
+ *  @param diff The policy difference structure from which to get the
+ *  number of removed classes.
  *  @return the number of removed classes or 0 if not yet run.
  */
-extern size_t poldiff_class_diff_summary_get_num_removed_classes(poldiff_t *diff, poldiff_class_diff_summary_t *cds);
+extern size_t poldiff_get_num_removed_classes(poldiff_t *diff);
+
 /**
- *  Get the number of mocified classes from a class difference summary;
- *  @param diff The policy difference structure associated with the
- *  class difference summary.
- *  @param cds The class difference summary from which to get the number
- *  of modified classes.
+ *  Get the number of modified classes from a policy difference structure.
+ *  @param diff The policy difference structure from which to get the
+ *  number ov modified classes.
  *  @return the number of modified classes or 0 if not yet run.
  */
-extern size_t poldiff_class_diff_summary_get_num_modified_classes(poldiff_t *diff, poldiff_class_diff_summary_t *cds);
+extern size_t poldiff_get_num_modified_classes(poldiff_t *diff);
+
 /**
- *  Get the vector of class differences from the class difference summary;
+ *  Get the vector of class differences from the class difference summary.
  *  @param diff The policy difference structure associated with the
  *  class difference summary.
- *  @param cds The class difference summary from which to get the vector
- *  of class differences.
- *  @return a vector of elements of type class_diff_t, or NULL on error.
+ *  @return a vector of elements of type class_t, or NULL on error.
  *  The caller should <b>not</b> destroy the vector returned. If the call
  *  fails, errno will be set.
  */
-extern apol_vector_t *poldiff_class_diff_summary_get_class_diff_vector(poldiff_t *diff, poldiff_class_diff_summary_t *cds);
+extern apol_vector_t *poldiff_get_class_vector(poldiff_t *diff);
 
 #endif /* POLDIFF_CLASS_DIFF_H */
