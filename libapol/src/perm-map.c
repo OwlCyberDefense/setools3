e7f409b91b46fe29866615cb60f80369c7889bdd
Jason Tang <jtang@tresys.com>
2006-08-23 16:41:40 +0000

    setools: removed a bunch of compiler warnings.  Bugz 1884

--- libapol/src/perm-map.c
+++ libapol/src/perm-map.c
@@ -24,6 +24,8 @@
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
+#include <config.h>
+
 #include <apol/perm-map.h>
 #include <apol/vector.h>
 #include <apol/util.h>
