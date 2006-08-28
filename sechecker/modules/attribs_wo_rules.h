3e7e5b0187cfc96ff32806ad2e2229b23f7cd8df
jmowery <jmowery@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2006-08-28 16:50:47 +0000

    SEChecker:    Incremental check-in of module format fix.    Note: SECHecker will not yet compile. Bugz: 1882

--- sechecker/modules/attribs_wo_rules.h
+++ sechecker/modules/attribs_wo_rules.h
@@ -1,9 +1,26 @@
-/* Copyright (C) 2005 Tresys Technology, LLC
- * see file 'COPYING' for use and warranty information */
- 
-/* 
- * Author: jmowery@tresys.com
+/**
+ *  @file attribs_wo_rules.h
+ *  Defines the interface for the attributes without rules module. 
  *
+ *  @author Kevin Carr kcarr@tresys.com
+ *  @author Jeremy A. Mowery jmowery@tresys.com
+ *  @author Jason Tang jtang@tresys.com
+ *
+ *  Copyright (C) 2005-2006 Tresys Technology, LLC
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
 #ifndef ATTRIBS_WO_RULES
@@ -16,11 +33,6 @@
 #include <apol/terule-query.h>
 #include <apol/type-query.h>
 
-/* The attribs_wo_rules_data structure is used to hold the check specific
- *  private data of a module. */
-typedef struct attribs_wo_rules_data {
-} attribs_wo_rules_data_t;
-
 /* Module functions:
  * Do not change any of these prototypes or you will not be
  * able to run the module in the library
@@ -28,21 +40,15 @@ typedef struct attribs_wo_rules_data {
  * statically; this means that all modules and their functions
  * are in the same namespace. */
 int attribs_wo_rules_register(sechk_lib_t *lib);
-int attribs_wo_rules_init(sechk_module_t *mod, apol_policy_t *policy);
-int attribs_wo_rules_run(sechk_module_t *mod, apol_policy_t *policy);
-void attribs_wo_rules_data_free(void *data);
-int attribs_wo_rules_print_output(sechk_module_t *mod, apol_policy_t *policy);
-sechk_result_t *attribs_wo_rules_get_result(sechk_module_t *mod);
+int attribs_wo_rules_init(sechk_module_t *mod, apol_policy_t *policy, void *arg);
+int attribs_wo_rules_run(sechk_module_t *mod, apol_policy_t *policy, void *arg);
+int attribs_wo_rules_print(sechk_module_t *mod, apol_policy_t *policy, void *arg);
 
 /* NOTE: While SEChecker is build statically, it is
  * intended that no module directly call a function
  * from another but instead use get_module_function()
  * to get the desired function from the library. */
 
-int attribs_wo_rules_get_list(sechk_module_t *mod, apol_vector_t **array);
-
-/* The following function is used to allocate and initialize
- * the private data storage structure for this module */
-attribs_wo_rules_data_t *attribs_wo_rules_data_new(void);
+int attribs_wo_rules_get_list(sechk_module_t *mod, apol_policy_t *policy, void *arg);
 
 #endif
--- sechecker/modules/domain_and_file.c
+++ sechecker/modules/domain_and_file.c
@@ -1,10 +1,28 @@
-/* Copyright (C) 2005 Tresys Technology, LLC
- * see file 'COPYING' for use and warranty information */
- 
-/* 
- * Author: jmowery@tresys.com
+/**
+ *  @file domain_and_file.c
+ *  Implementation of the domain and file type module. 
  *
+ *  @author Kevin Carr kcarr@tresys.com
+ *  @author Jeremy A. Mowery jmowery@tresys.com
+ *  @author Jason Tang jtang@tresys.com
+ *
+ *  Copyright (C) 2005-2006 Tresys Technology, LLC
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+
 
 #include "domain_and_file.h"
 
@@ -20,34 +38,34 @@ int domain_and_file_register(sechk_lib_t *lib)
 	sechk_fn_t *fn_struct = NULL;
 
 	if (!lib) {
-                ERR(NULL, "%s", "No library");
+		ERR(NULL, "%s", "No library");
 		return -1;
 	}
 
 	mod = sechk_lib_get_module(mod_name, lib);
 	if (!mod) {
-                ERR(NULL, "%s", "Module unknown");
+		ERR(NULL, "%s", "Module unknown");
 		return -1;
 	}
 	mod->parent_lib = lib;
-	
+
 	/* assign descriptions */
 	mod->brief_description = "types treated as a domain and file type";
 	mod->detailed_description = 
-"--------------------------------------------------------------------------------\n"
-"This module finds all types in the policy treated as both a domain and a file   \n"
-"type.  See find_domains and find_file_types modules for details about the       \n"
-"heuristics used to determine these types.  It is considered bad security\n"
-"practice to use the same type for a domain and its data objects because it \n"
-"requires that less restrictive access be granted to these types.\n";
+		"--------------------------------------------------------------------------------\n"
+		"This module finds all types in the policy treated as both a domain and a file   \n"
+		"type.  See find_domains and find_file_types modules for details about the       \n"
+		"heuristics used to determine these types.  It is considered bad security\n"
+		"practice to use the same type for a domain and its data objects because it \n"
+		"requires that less restrictive access be granted to these types.\n";
 	mod->opt_description = 
-"Module requirements:\n"
-"   none\n"
-"Module dependencies:\n"
-"   find_domains module\n"
-"   find_file_types module\n"
-"Module options:\n"
-"   none\n";
+		"Module requirements:\n"
+		"   none\n"
+		"Module dependencies:\n"
+		"   find_domains module\n"
+		"   find_file_types module\n"
+		"Module options:\n"
+		"   none\n";
 	mod->severity = SECHK_SEV_LOW;
 	/* assign requirements */
 	apol_vector_append(mod->requirements, sechk_name_value_new("policy_type", "source"));
@@ -59,93 +77,61 @@ int domain_and_file_register(sechk_lib_t *lib)
 	/* register functions */
 	fn_struct = sechk_fn_new();
 	if (!fn_struct) {
-                ERR(NULL, "%s", strerror(ENOMEM));
+		ERR(NULL, "%s", strerror(ENOMEM));
 		return -1;
 	}
 	fn_struct->name = strdup(SECHK_MOD_FN_INIT);
 	if (!fn_struct->name) {
-                ERR(NULL, "%s", strerror(ENOMEM));
+		ERR(NULL, "%s", strerror(ENOMEM));
 		return -1;
 	}
 	fn_struct->fn = &domain_and_file_init;
-        if ( apol_vector_append(mod->functions, (void*)fn_struct) < 0 ) {
-                ERR(NULL, "%s", strerror(ENOMEM));
-                return -1;
-        }
+	if ( apol_vector_append(mod->functions, (void*)fn_struct) < 0 ) {
+		ERR(NULL, "%s", strerror(ENOMEM));
+		return -1;
+	}
 
 	fn_struct = sechk_fn_new();
 	if (!fn_struct) {
-                ERR(NULL, "%s", strerror(ENOMEM));
+		ERR(NULL, "%s", strerror(ENOMEM));
 		return -1;
 	}
 	fn_struct->name = strdup(SECHK_MOD_FN_RUN);
 	if (!fn_struct->name) {
-                ERR(NULL, "%s", strerror(ENOMEM));
+		ERR(NULL, "%s", strerror(ENOMEM));
 		return -1;
 	}
-	fn_struct->fn = &domain_and_file_run;
-        if ( apol_vector_append(mod->functions, (void*)fn_struct) < 0 ) {
-                ERR(NULL, "%s", strerror(ENOMEM));
-                return -1;
-        }
+	fn_struct->fn = domain_and_file_run;
+	if ( apol_vector_append(mod->functions, (void*)fn_struct) < 0 ) {
+		ERR(NULL, "%s", strerror(ENOMEM));
+		return -1;
+	}
+
+	mod->data_free = NULL;
 
 	fn_struct = sechk_fn_new();
 	if (!fn_struct) {
-                ERR(NULL, "%s", strerror(ENOMEM));
-		return -1;
-	}
-	fn_struct->name = strdup(SECHK_MOD_FN_FREE);
-	if (!fn_struct->name) {
-                ERR(NULL, "%s", strerror(ENOMEM));
-		return -1;
-	}
-	fn_struct->fn = &domain_and_file_data_free;
-        if ( apol_vector_append(mod->functions, (void*)fn_struct) < 0 ) {
-                ERR(NULL, "%s", strerror(ENOMEM));
-                return -1;
-        }
-
-	fn_struct = sechk_fn_new();
-	if (!fn_struct) {
-                ERR(NULL, "%s", strerror(ENOMEM));
+		ERR(NULL, "%s", strerror(ENOMEM));
 		return -1;
 	}
 	fn_struct->name = strdup(SECHK_MOD_FN_PRINT);
 	if (!fn_struct->name) {
-                ERR(NULL, "%s", strerror(ENOMEM));
+		ERR(NULL, "%s", strerror(ENOMEM));
 		return -1;
 	}
-	fn_struct->fn = &domain_and_file_print_output;
-        if ( apol_vector_append(mod->functions, (void*)fn_struct) < 0 ) {
-                ERR(NULL, "%s", strerror(ENOMEM));
-                return -1;
-        }
-
-	fn_struct = sechk_fn_new();
-	if (!fn_struct) {
-                ERR(NULL, "%s", strerror(ENOMEM));
+	fn_struct->fn = domain_and_file_print;
+	if ( apol_vector_append(mod->functions, (void*)fn_struct) < 0 ) {
+		ERR(NULL, "%s", strerror(ENOMEM));
 		return -1;
 	}
-	fn_struct->name = strdup(SECHK_MOD_FN_GET_RES);
-	if (!fn_struct->name) {
-                ERR(NULL, "%s", strerror(ENOMEM));
-		return -1;
-	}
-	fn_struct->fn = &domain_and_file_get_result;
-        if ( apol_vector_append(mod->functions, (void*)fn_struct) < 0 ) {
-                ERR(NULL, "%s", strerror(ENOMEM));
-                return -1;
-        }
 
 	return 0;
 }
 
-int domain_and_file_init(sechk_module_t *mod, apol_policy_t *policy) 
+int domain_and_file_init(sechk_module_t *mod, apol_policy_t *policy, void *arg __attribute__((unused)))
 {
-	domain_and_file_data_t *datum = NULL;
-
 	if (!mod || !policy) {
-                ERR(policy, "%s", "Invalid parameters");
+		ERR(policy, "%s", "Invalid parameters");
 		return -1;
 	}
 	if (strcmp(mod_name, mod->name)) {
@@ -153,32 +139,25 @@ int domain_and_file_init(sechk_module_t *mod, apol_policy_t *policy)
 		return -1;
 	}
 
-	datum = domain_and_file_data_new();
-	if (!datum) {
-                ERR(policy, "%s", strerror(ENOMEM));
-		return -1;
-	}
-	mod->data = datum;
+	mod->data = NULL;
 
 	return 0;
 }
 
-int domain_and_file_run(sechk_module_t *mod, apol_policy_t *policy) 
+int domain_and_file_run(sechk_module_t *mod, apol_policy_t *policy, void *arg __attribute__((unused)))
 {
-	domain_and_file_data_t *datum;
 	sechk_result_t *res = NULL;
 	sechk_item_t *item = NULL;
 	sechk_proof_t *proof = NULL;
 	sechk_result_t *domain_res = NULL, *file_type_res = NULL;
 	size_t i, j, k;
-	sechk_run_fn_t run_fn = NULL;
-	sechk_get_result_fn_t get_res = NULL;
+	sechk_mod_fn_t run_fn = NULL;
 	sechk_name_value_t *dep = NULL;
 	apol_vector_t *domain_vector;
 	apol_vector_t *type_vector;
 
 	if (!mod || !policy) {
-                ERR(policy, "%s", "Invalid parameters");
+		ERR(policy, "%s", "Invalid parameters");
 		return -1;
 	}
 	if (strcmp(mod_name, mod->name)) {
@@ -190,52 +169,41 @@ int domain_and_file_run(sechk_module_t *mod, apol_policy_t *policy)
 	if (mod->result)
 		return 0;
 
-	datum = (domain_and_file_data_t*)mod->data;
 	res = sechk_result_new();
 	if (!res) {
-                ERR(policy, "%s", strerror(ENOMEM));
+		ERR(policy, "%s", strerror(ENOMEM));
 		return -1;
 	}
 	res->test_name = strdup(mod_name);
 	if (!res->test_name) {
-                ERR(policy, "%s", strerror(ENOMEM));
+		ERR(policy, "%s", strerror(ENOMEM));
 		goto domain_and_file_run_fail;
 	}
 	res->item_type = SECHK_ITEM_TYPE;
-        if ( !(res->items = apol_vector_create()) ) {
-                ERR(policy, "%s", strerror(ENOMEM));
-                goto domain_and_file_run_fail;
-        }
+	if ( !(res->items = apol_vector_create()) ) {
+		ERR(policy, "%s", strerror(ENOMEM));
+		goto domain_and_file_run_fail;
+	}
 
 	/* run dependencies */
 	for (i=0;i<apol_vector_get_size(mod->dependencies); i++) {
 		dep = apol_vector_get_element(mod->dependencies, i);
 		run_fn = sechk_lib_get_module_function(dep->value, SECHK_MOD_FN_RUN, mod->parent_lib);
-		run_fn(sechk_lib_get_module(dep->value, mod->parent_lib), policy);
+		run_fn(sechk_lib_get_module(dep->value, mod->parent_lib), policy, NULL);
 	}
 
 	/* get results */
-	get_res = sechk_lib_get_module_function("find_domains", SECHK_MOD_FN_GET_RES, mod->parent_lib);
-	if (!get_res) {
-                ERR(policy, "%s", "Unable to find get_result function for module find_domains");
-		goto domain_and_file_run_fail;
-	}
-	domain_res = get_res(sechk_lib_get_module("find_domains", mod->parent_lib));
+	domain_res = sechk_lib_get_module_result("find_domains", mod->parent_lib);
 	if (!domain_res) {
-                ERR(policy, "%s", "Unable to get results for module find_domains");
+		ERR(policy, "%s", "Unable to get results for module find_domains");
 		goto domain_and_file_run_fail;
 	}
-	get_res = sechk_lib_get_module_function("find_file_types", SECHK_MOD_FN_GET_RES, mod->parent_lib);
-	if (!get_res) {
-                ERR(policy, "%s", "Unable to find get_result function for module find_file_types");
-		goto domain_and_file_run_fail;
-	}
-	file_type_res = get_res(sechk_lib_get_module("find_file_types", mod->parent_lib));
+	file_type_res = sechk_lib_get_module_result("find_file_types", mod->parent_lib);
 	if (!file_type_res) {
-                ERR(policy, "%s", "Unable to get results for module find_file_types");
+		ERR(policy, "%s", "Unable to get results for module find_file_types");
 		goto domain_and_file_run_fail;
 	}
-	
+
 	/* get lists */
 	domain_vector = (apol_vector_t *)domain_res->items;
 	type_vector = (apol_vector_t *)file_type_res->items;
@@ -245,7 +213,7 @@ int domain_and_file_run(sechk_module_t *mod, apol_policy_t *policy)
 		sechk_item_t *type_item;
 		qpol_type_t *type;	
 		char *type_name;
-		
+
 		type_item = apol_vector_get_element(type_vector, i);
 		type = type_item->item;
 		qpol_type_get_name(policy->qh, policy->p, type, &type_name);
@@ -253,65 +221,65 @@ int domain_and_file_run(sechk_module_t *mod, apol_policy_t *policy)
 			sechk_item_t *domain_item;
 			qpol_type_t *domain;
 			char *domain_name;
-			
+
 			domain_item = apol_vector_get_element(domain_vector, j);
 			domain = domain_item->item;
 			qpol_type_get_name(policy->qh, policy->p, domain, &domain_name);
 			if (!strcmp(domain_name, type_name)) {
-		                item = sechk_item_new(NULL);
-                		if (!item) {
-			                ERR(policy, "%s", strerror(ENOMEM));
-                        		goto domain_and_file_run_fail;
-                		}
-                		item->item = (void *)domain;
-                        	if ( !(item->proof = apol_vector_create()) ) {
-			                ERR(policy, "%s", strerror(ENOMEM));
-                                	goto domain_and_file_run_fail;
-                        	}
+				item = sechk_item_new(NULL);
+				if (!item) {
+					ERR(policy, "%s", strerror(ENOMEM));
+					goto domain_and_file_run_fail;
+				}
+				item->item = (void *)domain;
+				if ( !(item->proof = apol_vector_create()) ) {
+					ERR(policy, "%s", strerror(ENOMEM));
+					goto domain_and_file_run_fail;
+				}
 				for ( k = 0; k < apol_vector_get_size(domain_item->proof); k++ ) {
 					sechk_proof_t *domain_proof;
-					
+
 					domain_proof = apol_vector_get_element(domain_item->proof, k);
 					proof = sechk_proof_new(NULL);
 					if (!proof) {
-				                ERR(policy, "%s", strerror(ENOMEM));
-                                        	goto domain_and_file_run_fail;
+						ERR(policy, "%s", strerror(ENOMEM));
+						goto domain_and_file_run_fail;
 					}
 					proof->type = SECHK_ITEM_TYPE;
 					proof->text = strdup(domain_proof->text);
 					if ( !proof->text) {
-				                ERR(policy, "%s", strerror(ENOMEM));
-                	                        goto domain_and_file_run_fail;
+						ERR(policy, "%s", strerror(ENOMEM));
+						goto domain_and_file_run_fail;
 					}
 					if ( apol_vector_append(item->proof, (void *)proof) < 0 ) {
-				               ERR(policy, "%s", strerror(ENOMEM));
- 	                                       goto domain_and_file_run_fail;
+						ERR(policy, "%s", strerror(ENOMEM));
+						goto domain_and_file_run_fail;
 					}
 				}
-                                for ( k = 0; k < apol_vector_get_size(type_item->proof); k++ ) {
-                                        sechk_proof_t *type_proof;
+				for ( k = 0; k < apol_vector_get_size(type_item->proof); k++ ) {
+					sechk_proof_t *type_proof;
 
-                                        type_proof = apol_vector_get_element(type_item->proof, k);
-                                        proof = sechk_proof_new(NULL);
-                                        if (!proof) {
-				                ERR(policy, "%s", strerror(ENOMEM));
-                                                goto domain_and_file_run_fail;
-                                        }
-                                        proof->type = SECHK_ITEM_TYPE;
-                                        proof->text = strdup(type_proof->text);
-                                        if ( !proof->text) {
-				                ERR(policy, "%s", strerror(ENOMEM));
-                                                goto domain_and_file_run_fail;
-                                        }
-                                        if ( apol_vector_append(item->proof, (void *)proof) < 0 ) {
-				               ERR(policy, "%s", strerror(ENOMEM));
-                                               goto domain_and_file_run_fail;
-                                        }
-                                }
-	                        if ( apol_vector_append(res->items, (void*)item) < 0 ) {
-			                ERR(policy, "%s", strerror(ENOMEM));
-                        	        goto domain_and_file_run_fail;
-	                        }
+					type_proof = apol_vector_get_element(type_item->proof, k);
+					proof = sechk_proof_new(NULL);
+					if (!proof) {
+						ERR(policy, "%s", strerror(ENOMEM));
+						goto domain_and_file_run_fail;
+					}
+					proof->type = SECHK_ITEM_TYPE;
+					proof->text = strdup(type_proof->text);
+					if ( !proof->text) {
+						ERR(policy, "%s", strerror(ENOMEM));
+						goto domain_and_file_run_fail;
+					}
+					if ( apol_vector_append(item->proof, (void *)proof) < 0 ) {
+						ERR(policy, "%s", strerror(ENOMEM));
+						goto domain_and_file_run_fail;
+					}
+				}
+				if ( apol_vector_append(res->items, (void*)item) < 0 ) {
+					ERR(policy, "%s", strerror(ENOMEM));
+					goto domain_and_file_run_fail;
+				}
 			}
 		}
 	}
@@ -331,9 +299,8 @@ void domain_and_file_data_free(void *data)
 	free(data);
 }
 
-int domain_and_file_print_output(sechk_module_t *mod, apol_policy_t *policy) 
+int domain_and_file_print(sechk_module_t *mod, apol_policy_t *policy, void *arg __attribute__((unused))) 
 {
-	domain_and_file_data_t *datum = NULL;
 	unsigned char outformat = 0x00;
 	sechk_item_t *item = NULL;
 	sechk_proof_t *proof = NULL;
@@ -341,8 +308,8 @@ int domain_and_file_print_output(sechk_module_t *mod, apol_policy_t *policy)
 	qpol_type_t *type;
 	char *type_name;
 
-        if (!mod || !policy){
-                ERR(policy, "%s", "Invalid parameters");
+	if (!mod || !policy){
+		ERR(policy, "%s", "Invalid parameters");
 		return -1;
 	}
 	if (strcmp(mod_name, mod->name)) {
@@ -350,12 +317,11 @@ int domain_and_file_print_output(sechk_module_t *mod, apol_policy_t *policy)
 		return -1;
 	}
 
-	datum = (domain_and_file_data_t*)mod->data;
 	outformat = mod->outputformat;
 	num_items = apol_vector_get_size(mod->result->items);
 
 	if (!mod->result) {
-                ERR(policy, "%s", "Module has not been run");
+		ERR(policy, "%s", "Module has not been run");
 		return -1;
 	}
 
@@ -366,58 +332,36 @@ int domain_and_file_print_output(sechk_module_t *mod, apol_policy_t *policy)
 		printf("Found %i types.\n", num_items);
 	}
 	if (outformat & SECHK_OUT_LIST) {
-                printf("\n");
-                for (i=0;i<num_items;i++) {
-                        j++;
-                        j %= 4;
-                        item = apol_vector_get_element(mod->result->items, i);
-                        type = (qpol_type_t*)item->item;
-                        qpol_type_get_name(policy->qh, policy->p, type, &type_name);
-                        printf("%s%s", type_name, (char *)( (j && i!=num_items-1) ? ", " : "\n"));
-                }
-                printf("\n");
+		printf("\n");
+		for (i=0;i<num_items;i++) {
+			j++;
+			j %= 4;
+			item = apol_vector_get_element(mod->result->items, i);
+			type = (qpol_type_t*)item->item;
+			qpol_type_get_name(policy->qh, policy->p, type, &type_name);
+			printf("%s%s", type_name, (char *)( (j && i!=num_items-1) ? ", " : "\n"));
+		}
+		printf("\n");
 	}
 
 	if (outformat & SECHK_OUT_PROOF) {
-                printf("\n");
-                for (k=0;k<num_items;k++) {
-                        item = apol_vector_get_element(mod->result->items, k);
-                        if ( item ) {
-                                type = item->item;
-                                qpol_type_get_name(policy->qh, policy->p, type, &type_name);
-                                printf("%s\n", (char*)type_name);
-                                for (l = 0; l < apol_vector_get_size(item->proof); l++) {
-                                        proof = apol_vector_get_element(item->proof,l);
-                                        if ( proof )
-                                                printf("\t%s\n", proof->text);
-                                }
-                        }
-                }
-                printf("\n");
+		printf("\n");
+		for (k=0;k<num_items;k++) {
+			item = apol_vector_get_element(mod->result->items, k);
+			if ( item ) {
+				type = item->item;
+				qpol_type_get_name(policy->qh, policy->p, type, &type_name);
+				printf("%s\n", (char*)type_name);
+				for (l = 0; l < apol_vector_get_size(item->proof); l++) {
+					proof = apol_vector_get_element(item->proof,l);
+					if ( proof )
+						printf("\t%s\n", proof->text);
+				}
+			}
+		}
+		printf("\n");
 	}
 
 	return 0;
 }
 
-sechk_result_t *domain_and_file_get_result(sechk_module_t *mod) 
-{
-	if (!mod) {
-                ERR(NULL, "%s", "Invalid parameters");
-		return NULL;
-	}
-	if (strcmp(mod_name, mod->name)) {
-                ERR(NULL, "Wrong module (%s)", mod->name);
-		return NULL;
-	}
-
-	return mod->result;
-}
-
-domain_and_file_data_t *domain_and_file_data_new(void) 
-{
-	domain_and_file_data_t *datum = NULL;
-
-	datum = (domain_and_file_data_t*)calloc(1,sizeof(domain_and_file_data_t));
-
-	return datum;
-}
