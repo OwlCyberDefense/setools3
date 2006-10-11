c6251dad50cc0692357476ea83ae8ebfb7f26223
Jason Tang <jtang@tresys.com>
2006-10-11 17:41:28 +0000

    apol: apol no longer uses -p flag to specify a policy (-p remains as an undocumented option for compatibility) fix missing backslashes in notebook compatibility code Bugz 1908

--- apol/apol_gui.c
+++ apol/apol_gui.c
@@ -1,7 +1,7 @@
  /* Copyright (C) 2001-2006 Tresys Technology, LLC
  * see file 'COPYING' for use and warranty information */
 
-/* 
+/*
  * Author: mayerf@tresys.com and Don Patterson <don.patterson@tresys.com>
  */
 
@@ -25,7 +25,7 @@
 
 /* internal global */
 char* policy_conf_file;
-static struct option const opts[] = 
+static struct option const opts[] =
 {
 	{"help", no_argument, NULL, 'h'},
 	{"version", no_argument, NULL, 'v'},
@@ -37,7 +37,7 @@ int Tcl_AppInit(Tcl_Interp *interp)
 {
 	char *script;
 	Tcl_DString command;
-	int rt;	
+	int rt;
 
 	if (Tcl_Init(interp) == TCL_ERROR) {
 		return TCL_ERROR;
@@ -47,26 +47,26 @@ int Tcl_AppInit(Tcl_Interp *interp)
 	}
 	/* apolicy packagae initialization */
 	if (apol_tcl_init(interp) == TCL_ERROR) {
-     		return TCL_ERROR;
-     	}
-     	
-     	/* find and start the TCL scripts */
-     	if (apol_tcl_get_startup_script(interp, STARTUP_SCRIPT) != TCL_OK) {
-     		fprintf(stderr, "Error finding TCL script: %s\n", interp->result);
-     		Tcl_DeleteInterp(interp);
-     		Tcl_Exit(1);
-     	}
-     	script = (char *)malloc(strlen(interp->result) + strlen(STARTUP_SCRIPT) + 3);
-     	if(script == NULL) {
-     		fprintf(stderr, "out of memory\n");
-     		Tcl_DeleteInterp(interp);
-     		Tcl_Exit(1);
-     	}
-     	sprintf(script, "%s/%s", interp->result, STARTUP_SCRIPT);
+		return TCL_ERROR;
+	}
+
+	/* find and start the TCL scripts */
+	if (apol_tcl_get_startup_script(interp, STARTUP_SCRIPT) != TCL_OK) {
+		fprintf(stderr, "Error finding TCL script: %s\n", interp->result);
+		Tcl_DeleteInterp(interp);
+		Tcl_Exit(1);
+	}
+	script = (char *)malloc(strlen(interp->result) + strlen(STARTUP_SCRIPT) + 3);
+	if(script == NULL) {
+		fprintf(stderr, "out of memory\n");
+		Tcl_DeleteInterp(interp);
+		Tcl_Exit(1);
+	}
+	sprintf(script, "%s/%s", interp->result, STARTUP_SCRIPT);
 	if(Tcl_EvalFile(interp, script) != TCL_OK) {
 		fprintf(stderr, "Error in StartScript (%s): %s on line %d\n\nIf %s is set, make sure directory is correct\n", script, interp->result, interp->errorLine, APOL_ENVIRON_VAR_NAME);
-	  	Tcl_DeleteInterp(interp);
-	  	Tcl_Exit(1);
+		Tcl_DeleteInterp(interp);
+		Tcl_Exit(1);
 	}
 
 	/* If a policy.conf file was provided on command line, open it */
@@ -77,20 +77,20 @@ int Tcl_AppInit(Tcl_Interp *interp)
 		Tcl_DStringAppend(&command, " 0", strlen(" 0"));
 		rt = Tcl_Eval(interp, Tcl_DStringValue(&command));
 		Tcl_DStringFree(&command);
-     	}
+	}
 	free(script);
-    	return TCL_OK;
+	return TCL_OK;
 }
 
-void usage(const char *program_name, bool_t brief) 
+void usage(const char *program_name, bool_t brief)
 {
-	printf("Usage: %s [OPTIONS]\n", program_name);
+	printf("Usage: %s [OPTIONS] [FILE]\n", program_name);
 	if (brief) {
 		printf("   Try %s --help for more help.\n\n", program_name);
 		return;
 	}
 	printf("Policy Analysis tool for Security Enhanced Linux.\n\n");
-	printf("   -p FILE, --policy FILE  open policy file named FILE\n");
+	printf("   FILE                    open policy file named FILE\n");
 	printf("   -v, --version           display version information\n");
 	printf("   -h, --help              display this help dialog\n\n");
 	return;
@@ -113,7 +113,7 @@ void parse_command_line(int argc, char **argv)
 	while ((optc = getopt_long(argc, argv, "p:vh", opts, NULL)) != -1)
 	{
 		switch(optc) {
-		case 'p':
+		case 'p':   /* -p is deprecated and should not be used */
 			policy_conf_file = optarg;
 			break;
 		case 'h':
@@ -137,11 +137,7 @@ void parse_command_line(int argc, char **argv)
 		exit(1);
 	}
 	if (optind < argc) { /* trailing non-options */
-		printf("non-option arguments: ");
-		while (optind < argc)
-			printf("%s ", argv[optind++]);
-		printf("\n");
-		exit(1);
+		policy_conf_file = argv[optind];
 	}
 }
 
@@ -151,21 +147,21 @@ int main(int argc, char *argv[])
 
 	parse_command_line(argc, argv);
 	interp = Tcl_CreateInterp();
-	/* Compute the full path name of the executable file from which 
-	 * the application was invoked and save it for Tcl's internal 
+	/* Compute the full path name of the executable file from which
+	 * the application was invoked and save it for Tcl's internal
 	 * use. */
 	Tcl_FindExecutable(argv[0]);
 
 	/* Normally, the function 'Tk_MainEx(1, argv, Tcl_AppInit, interp);'
 	 * would have been called instead of the following 'if' statement,
-	 * however, this would cause apol to run wish in interactive mode, 
-	 * which is usually used for debugging purposes and this causes a 
-	 * strange interaction with normal shell operations (backgrounding, 
-	 * etc). 
+	 * however, this would cause apol to run wish in interactive mode,
+	 * which is usually used for debugging purposes and this causes a
+	 * strange interaction with normal shell operations (backgrounding,
+	 * etc).
 	 */
-	if (Tcl_AppInit(interp) == TCL_OK) 
+	if (Tcl_AppInit(interp) == TCL_OK)
 		Tk_MainLoop();
-	
+
 	/* Exit after the event loop returns. */
-  	exit(0);
+	exit(0);
 }
