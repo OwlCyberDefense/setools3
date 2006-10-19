bdf994153620f5e65472b566c01f102358434f65
jtang <jtang@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2006-10-19 22:26:38 +0000

    added libpoldiff_get_version(). added new library calls to their respective linker maps

---
+++ libpoldiff/src/util.c
@@ -0,0 +1,31 @@
+/**
+ * @file util.c
+ *
+ * Implementation of utility functions.
+ *
+ * @author Jeremy A. Mowery jmowery@tresys.com
+ * @author Jason Tang  jtang@tresys.com
+ *
+ * Copyright (C) 2006 Tresys Technology, LLC
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+#include <config.h>
+
+const char* libpoldiff_get_version(void)
+{
+        return LIBPOLDIFF_VERSION_STRING;
+}
