e75d127fa40e615cdc05c5fe47fe7c1902ebf266
Jason Mowery <jmowery@tresys.com>
2008-10-02 15:08:40 +0000

    apparently sepol/errcodes.h does not always exist depending on version of libsepol

--- libqpol/src/policy_extend.c
+++ libqpol/src/policy_extend.c
@@ -32,7 +32,9 @@
 #include <sepol/policydb/flask.h>
 #include <sepol/policydb/ebitmap.h>
 #include <sepol/policydb/expand.h>
+#ifdef HAVE_SEPOL_ERRCODES
 #include <sepol/errcodes.h>
+#endif
 #include <qpol/policy.h>
 #include <qpol/policy_extend.h>
 #include <qpol/iterator.h>
