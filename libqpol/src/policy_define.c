268f24b096b671f6074173fd80a322e48b38bf02
cpebenito <cpebenito@c5f68610-3bfa-0310-bcc3-b3c1dee5e5c1>
2009-07-09 19:12:33 +0000

    remove accidental bits of previous commit.

--- libqpol/src/policy_define.c
+++ libqpol/src/policy_define.c
@@ -57,9 +57,7 @@
 #ifdef HAVE_SEPOL_POLICYCAPS
 #include <sepol/policydb/polcaps.h>
 #endif
-#ifdef HAVE_SEPOL_ERRCODES
 #include <sepol/errcodes.h>
-#endif
 
 #include "queue.h"
 #include <qpol/policy.h>
